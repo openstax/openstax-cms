c6b07effffef0b092b8c0f89f2e2a378408ad469
Michael Harrison <mwharrison@rice.edu>
2016-02-09 14:21:53 -0600

    clean account urls - remove string refrences to views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url, include
 
+from . import views
+
 
 urlpatterns = [
-    url(r'^$', 'accounts.views.home'),
-    url(r'^login/$', 'accounts.views.login',name='login'),
-    url(r'^logout/$', 'accounts.views.logout',name='logout'),
-    url(r'^done/$', 'accounts.views.done', name='done'),
-    url(r'^profile/$', 'accounts.views.profile', name='profile'),
-    url(r'^auth/$', 'accounts.views.auth',name='auth'),
+    url(r'^$', views.home),
+    url(r'^login/$', views.login,name='login'),
+    url(r'^logout/$', views.logout,name='logout'),
+    url(r'^done/$', views.done, name='done'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^auth/$', views.auth,name='auth'),
     url(r'', include('social.apps.django_app.urls', namespace='social')),
 ]
