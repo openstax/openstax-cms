88f21371ee2c3f2e22ee093e40300499f319d5f0
Michael Harrison <mwharrison@rice.edu>
2016-02-22 15:24:24 -0600

    make ally categories boolean to allow for more than one selection

--- allies/models.py
+++ allies/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
 
 
 class Ally(models.Model):
-    ALLY_CATEGORY = (
-        ('OH', 'Online Homework'),
-        ('AC', 'Adaptive Courseware'),
-        ('CT', 'Customized Tools'),
-    )
-    ally_category = models.CharField(max_length=2,
-                        choices=ALLY_CATEGORY)
+    online_course = models.BooleanField(default=False)
+    adaptive_course = models.BooleanField(default=False)
+    customized_tools = models.BooleanField(default=False)
+
     logo = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -29,13 +24,22 @@ class Ally(models.Model):
     link_url = models.URLField(blank=True, help_text="Call to Action Link")
     link_text = models.CharField(max_length=255, help_text="Call to Action Text")
 
-    api_fields = ('ally_category', 'logo', 'heading', 'description', )
+    api_fields = ('online_course', 'adaptive_course', 'customized_tools', 'logo', 'heading',
+                  'short_description', 'long_description' )
 
     panels = [
-        FieldPanel('ally_category'),
+        MultiFieldPanel(
+        [
+          FieldPanel('online_course'),
+          FieldPanel('adaptive_course'),
+          FieldPanel('customized_tools'),
+        ],
+          heading="Categories",
+        ),
         ImageChooserPanel('logo'),
         FieldPanel('heading'),
-        FieldPanel('description'),
+        FieldPanel('short_description'),
+        FieldPanel('long_description'),
     ]
 
     def __str__(self):
