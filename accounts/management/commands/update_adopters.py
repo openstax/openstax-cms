7057386c901fc38d4e163d1b0c2b31f94a855cd0
rich-hart <rich.hart@rice.edu>
2016-03-17 15:39:51 -0500

    load data into first adoptes pages in object list

--- accounts/management/commands/update_adopters.py
+++ accounts/management/commands/update_adopters.py
@@ -1,23 +1,25 @@
 from django.core.management.base import BaseCommand, CommandError
 from accounts.salesforce import Salesforce
 from adopters.models import Adopter
-
+from pages.models import AdoptersPage
 
 class Command(BaseCommand):
     help = "update adopters from salesforce.com"
 
-    def add_arguments(self, parser):
-        parser.add_argument('page_id', type=int)
-
     def handle(self, *args, **options):
-        with Salesforce() as sf:
-            salesforce_adopters_list = sf.adopters()
-            for salesforce_adopter in salesforce_adopters_list:
-                adopter, created = Adopter.objects.get_or_create(salesforce_id=salesforce_adopter['Id'],
-                                                                  name=salesforce_adopter['Name'],
-                                                                  description=salesforce_adopter['Description'],
-                                                                  website=salesforce_adopter['Website'],
-                                                                  page_id=options['page_id'],)
-                adopter.save() 
-        success = self.style.SUCCESS("Successfully updated adopters")
-        self.stdout.write(success)
+        if AdoptersPage.objects.exists():
+            adopter_page = AdoptersPage.objects.all()[0]
+            
+            with Salesforce() as sf:
+                salesforce_adopters_list = sf.adopters()
+                for salesforce_adopter in salesforce_adopters_list:
+                    adopter, created = Adopter.objects.get_or_create(salesforce_id=salesforce_adopter['Id'],
+                                                                     name=salesforce_adopter['Name'],
+                                                                     description=salesforce_adopter['Description'],
+                                                                     website=salesforce_adopter['Website'],
+                                                                     page_id=adopter_page.pk,)
+                    adopter.save()
+            responce = self.style.SUCCESS("Successfully updated adopters")
+        else: 
+            response = self.style.HTTP_NOT_FOUND("Adopters page does not exist")
+        self.stdout.write(responce)
