69c1a6805e828b66a4bc6cdb7cfb52d7b11e5408
Michael Harrison <mwharrison@rice.edu>
2016-03-23 16:08:37 -0500

    add api to the mail url

--- mail/tests.py
+++ mail/tests.py
@@ -8,9 +8,9 @@ class MailTest(TestCase):
         self.client = Client(HTTP_USER_AGENT='Mozilla/5.0')
 
     def test_send(self):
-        response = self.client.post('/mail/send_mail/', {'to_address': 'noreply@openstax.org',
-                                                         'from_name': 'Openstax',
-                                                         'from_address': 'noreply@openstax.org',
-                                                         'subject': 'Test Subject',
-                                                         'message_body': 'This is a test.'})
+        response = self.client.post('/api/mail/send_mail/', {'to_address': 'noreply@openstax.org',
+                                                             'from_name': 'Openstax',
+                                                             'from_address': 'noreply@openstax.org',
+                                                             'subject': 'Test Subject',
+                                                             'message_body': 'This is a test.'})
         self.assertEqual(json.loads(response.content.decode('utf8')), {'sent': 1})
