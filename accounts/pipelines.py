9168bf146dc57da21988b5771f5e7193cec5df64
rich-hart <rich.hart@rice.edu>
2016-03-23 14:45:40 -0500

    add user import script

--- accounts/pipelines.py
+++ accounts/pipelines.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.conf import settings
 
-def save_profile(backend, user, response, *args, **kwargs):
+def save_profile( user, *args, **kwargs):
     #for now - setting email address to prevent issues, should update it eventually
     if user:
         user.email = '{}@openstax.org'.format(user.username)
