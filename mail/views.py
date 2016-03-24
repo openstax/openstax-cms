8a994ac6365274badcc57c6fa41857469a7935a2
Michael Harrison <mwharrison@rice.edu>
2016-03-24 14:42:20 -0500

    return json of message being sent to debug mail issue

--- mail/views.py
+++ mail/views.py
@@ -15,15 +15,25 @@ def send_contact_message(request):
         from_string = '{} <{}>'.format(from_name, from_address)
         subject = request.POST.get("subject", "")
         message_body = request.POST.get("message_body", "")
+        csrf_token = request.POST.get("csrfmiddlewaretoken", "")
 
         email = EmailMessage(subject,
                              message_body,
                              'noreply@openstax.org',
                              to_address,
                              reply_to=[from_string])
-        email.send()
+        email.send(fail_silently=False)
 
-        return redirect('/contact-thank-you')
+        #return redirect('/contact-thank-you')
+        data = {'subject': subject,
+                'message_body': message_body,
+                'to_address': to_address,
+                'reply_to': [from_string],
+                'from_address': 'noreply@openstax.org',
+                'csrf_token': csrf_token,
+                }
+
+        return JsonResponse(data)
     # if this is not posting a message, let's send the csfr token back
     else:
         csrf_token = csrf.get_token(request)
