4040ca2ca5466de696a5d6e708f5e36abd826be7
rich-hart <rich.hart@rice.edu>
2016-03-29 16:23:17 -0500

    add accounts id to user api

--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from wagtail.wagtailimages.models import Image
 from salesforce.models import Adopter
 from django.utils.six import StringIO
 from django.core.management import call_command
-
+from social.apps.django_app.default.models import DjangoStorage as SocialAuthStorage
 
 class AdopterViewSet(viewsets.ModelViewSet):
     queryset = Adopter.objects.all()
@@ -21,11 +21,19 @@ class UserView(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+
+        try:
+            social_auth = SocialAuthStorage.user.get_social_auth_for_user(user)
+            user.accounts_id = social_auth[0].uid
+        except:
+            user.accounts_id = None
+
         try:
             out = StringIO()
             call_command('update_faculty_status', str(user.pk), stdout=out)
         except:
             pass
+
         return [user]
 
 
