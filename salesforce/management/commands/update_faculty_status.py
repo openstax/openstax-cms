dc39cbc2c05e75756ae217e04aacf15d5ddc2a4a
rich-hart <rich.hart@rice.edu>
2016-03-30 12:16:32 -0500

    refactor salesforce commands

--- salesforce/management/commands/update_faculty_status.py
+++ salesforce/management/commands/update_faculty_status.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from salesforce.salesforce import Salesforce
 from social.apps.django_app.default.models import \
     DjangoStorage as SocialAuthStorage
-
+import argparse
 
 class Command(BaseCommand):
     help = "Add user to faculty group if confirmed by salesforce"
@@ -13,14 +13,24 @@ class Command(BaseCommand):
         parser.add_argument('--all', action='store_true', default=False)
 
     def handle(self, *args, **options):
-        if options['all']:
-            accounts_id = None
-        else:
-            social_user = SocialAuthStorage.user.objects.filter(
-                user_id=options['user_id'])
-            accounts_id = social_user[0].uid
         with Salesforce() as sf:
-            faculty_list = sf.faculty_status(accounts_id)
+            if options['all']:
+                command = "SELECT Accounts_ID__c FROM Contact "\
+                              "WHERE Faculty_Verified__c = 'Confirmed' "\
+                              "AND Accounts_ID__c != null"
+                response = sf.query_all(command)
+            elif options['user_id']:
+                social_user = SocialAuthStorage.user.objects.filter(
+                    user_id=options['user_id'])
+                accounts_id = social_user[0].uid
+                command = "SELECT Accounts_ID__c FROM Contact "\
+                              "WHERE Faculty_Verified__c = 'Confirmed' "\
+                              "AND Accounts_ID__c = '{}'".format(accounts_id)
+                response = sf.query(command)
+            else:
+                raise argparse.ArgumentError()
+            records = response['records']
+            faculty_list = [contact['Accounts_ID__c'] for contact in records]
             for account_id in faculty_list:
                 social_user = SocialAuthStorage.user.objects.filter(
                     uid=account_id)
