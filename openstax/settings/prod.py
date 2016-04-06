ac856aef62b31d7bf43dd89d446e272a1f247a2b
Michael Harrison <mwharrison@rice.edu>
2016-04-06 09:36:15 -0500

    add temporary settings for accounts to work

--- openstax/settings/prod.py
+++ openstax/settings/prod.py
@@ -24,6 +24,15 @@ EMAIL_BACKEND = 'django_ses.SESBackend'
 AWS_SES_REGION_NAME = 'us-west-2'
 AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
 
+# Openstax Accounts
+AUTHORIZATION_URL = 'https://accounts-qa.openstax.org/oauth/authorize'
+ACCESS_TOKEN_URL = 'https://accounts-qa.openstax.org/oauth/token'
+USER_QUERY = 'https://accounts-qa.openstax.org/api/user?'
+SOCIAL_AUTH_OPENSTAX_KEY = '0a3c6b8c21091873805181b4b2a42cdbabeec6f6871332b817f59fac37033537'
+SOCIAL_AUTH_OPENSTAX_SECRET = '40035a7f2a7948b33ffce370af3918d692b958a6cc195e8b57b1fbe621a88157'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = 'http://os-webview-dev.openstax.org'
+SOCIAL_AUTH_SANITIZE_REDIRECTS = False
+
 try:
     from .local import *
 except ImportError:
