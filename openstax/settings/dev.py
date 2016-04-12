faba27a0908ea90f2b90f2f04879da1cc6e8d025
Michael Harrison <mwharrison@rice.edu>
2016-04-12 16:52:51 -0500

    dev->accounts-test, move keys to deploy scripts

--- openstax/settings/dev.py
+++ openstax/settings/dev.py
@@ -23,14 +23,12 @@ MEDIA_URL = "https://%s/%s/media/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STORAGE_DIR)
 DEFAULT_FILE_STORAGE = 'openstax.custom_storages.MediaStorage'
 
 # Openstax Accounts
-AUTHORIZATION_URL = 'https://accounts-qa.openstax.org/oauth/authorize'
-ACCESS_TOKEN_URL = 'https://accounts-qa.openstax.org/oauth/token'
-USER_QUERY = 'https://accounts-qa.openstax.org/api/user?'
-SOCIAL_AUTH_OPENSTAX_KEY = '0a3c6b8c21091873805181b4b2a42cdbabeec6f6871332b817f59fac37033537'
-SOCIAL_AUTH_OPENSTAX_SECRET = '40035a7f2a7948b33ffce370af3918d692b958a6cc195e8b57b1fbe621a88157'
+AUTHORIZATION_URL = 'https://accounts-test.openstax.org/oauth/authorize'
+ACCESS_TOKEN_URL = 'https://accounts-test.openstax.org/oauth/token'
+USER_QUERY = 'https://accounts-test.openstax.org/api/user?'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = 'https://oscms-dev.openstax.org'
 SOCIAL_AUTH_SANITIZE_REDIRECTS = False
-OPENSTAX_ACCOUNTS_LOGOUT_URL = 'https://accounts-qa.openstax.org/signout'
+OPENSTAX_ACCOUNTS_LOGOUT_URL = 'https://accounts-test.openstax.org/signout'
 
 try:
     from .local import *
--- openstax/settings/prod.py
+++ openstax/settings/prod.py
@@ -28,8 +28,10 @@ AWS_SES_REGION_ENDPOINT = 'email.us-west-2.amazonaws.com'
 AUTHORIZATION_URL = 'https://accounts-qa.openstax.org/oauth/authorize'
 ACCESS_TOKEN_URL = 'https://accounts-qa.openstax.org/oauth/token'
 USER_QUERY = 'https://accounts-qa.openstax.org/api/user?'
+#### These need to be moved to oscms deploy scripts before prod
 SOCIAL_AUTH_OPENSTAX_KEY = '0a3c6b8c21091873805181b4b2a42cdbabeec6f6871332b817f59fac37033537'
 SOCIAL_AUTH_OPENSTAX_SECRET = '40035a7f2a7948b33ffce370af3918d692b958a6cc195e8b57b1fbe621a88157'
+####
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = 'https://cms.openstax.org'
 SOCIAL_AUTH_SANITIZE_REDIRECTS = False
 OPENSTAX_ACCOUNTS_LOGOUT_URL = 'https://accounts-qa.openstax.org/signout'
