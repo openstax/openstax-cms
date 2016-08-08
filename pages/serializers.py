0d2b70cda322f5c2a41551b72135cf5d069083ec
Michael Harrison <mwharrison@rice.edu>
2016-08-08 10:58:14 -0500

    use model defined api_fields on serializers

--- pages/serializers.py
+++ pages/serializers.py
@@ -2,88 +2,32 @@ from pages.models import HomePage, HigherEducation, GeneralPage, ContactUs, Abou
 
 from wagtail.contrib.wagtailapi.serializers import PageSerializer
 
+
 class HomePageSerializer(PageSerializer):
     class Meta:
         model = HomePage
-        fields = ('id',
-                  'title',
-                  'row_1',
-                  'row_2',
-                  'row_3',
-                  'row_4',
-                  'row_5',
-                  'slug',
-                  'seo_title',
-                  'search_description',
-                  )
+        fields = HomePage.api_fields
 
 
 class HigherEducationSerializer(PageSerializer):
     class Meta:
         model = HigherEducation
-        fields = ('id',
-                  'title',
-                  'intro_heading',
-                  'intro_description',
-                  'row_1',
-                  'get_started_heading',
-                  'get_started_step_1_heading',
-                  'get_started_step_1_description',
-                  'get_started_step_1_cta',
-                  'get_started_step_2_heading',
-                  'get_started_step_2_description',
-                  'get_started_step_2_cta',
-                  'get_started_step_3_heading',
-                  'get_started_step_3_description',
-                  'get_started_step_3_cta',
-                  'adopt_heading',
-                  'adopt_description',
-                  'adopt_cta',
-                  'row_2',
-                  'row_3',
-                  'slug',
-                  'seo_title',
-                  'search_description',
-                  )
+        fields = HigherEducation.api_fields
 
 
 class GeneralPageSerializer(PageSerializer):
     class Meta:
         model = GeneralPage
-        fields = ('id',
-                  'title',
-                  'body',
-                  'slug',
-                  'seo_title',
-                  'search_description',
-                  )
+        fields = GeneralPage.api_fields
 
 
 class ContactUsSerializer(PageSerializer):
     class Meta:
         model = ContactUs
-        fields = ('id',
-                  'title',
-                  'tagline',
-                  'mailing_header',
-                  'mailing_address',
-                  'phone_number',
-                  'slug',
-                  'seo_title',
-                  'search_description',
-                  )
+        fields = ContactUs.api_fields
 
 
 class AboutUsSerializer(PageSerializer):
     class Meta:
         model = AboutUs
-        fields = ('tagline',
-                  'intro_heading',
-                  'intro_paragraph',
-                  'our_team_heading',
-                  'openstax_team',
-                  'strategic_advisors',
-                  'slug',
-                  'seo_title',
-                  'search_description',
-                  )
+        fields = AboutUs.api_fields
