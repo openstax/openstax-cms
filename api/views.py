43ad339c4a5ad55b09129be205269fc4aa403853
Michael Harrison <mwharrison@rice.edu>
2016-08-24 10:00:42 -0500

    return object for user api, not list

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from social.apps.django_app.default.models import \
     DjangoStorage as SocialAuthStorage
 from wagtail.wagtailimages.models import Image
 
-from .serializers import AdopterSerializer, ImageSerializer, UserSerializer
+from .serializers import AdopterSerializer, ImageSerializer
 
 
 class AdopterViewSet(viewsets.ModelViewSet):
@@ -21,24 +21,28 @@ class ImageViewSet(viewsets.ModelViewSet):
     serializer_class = ImageSerializer
 
 
-class UserView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
+def user_api(request):
+    user = request.user
 
-    def get_queryset(self):
-        user = self.request.user
+    try:
+        social_auth = SocialAuthStorage.user.get_social_auth_for_user(user)
+        user.accounts_id = social_auth[0].uid
+    except:
+        user.accounts_id = None
 
-        try:
-            social_auth = SocialAuthStorage.user.get_social_auth_for_user(user)
-            user.accounts_id = social_auth[0].uid
-        except:
-            user.accounts_id = None
-
-        return [user]
+    return JsonResponse({
+        'username': user.username,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'is_staff': user.is_staff,
+        'is_superuser': user.is_superuser,
+        'groups': list(user.groups.values_list('name', flat=True)),
+        'accounts_id': user.accounts_id,
+    })
 
 
 def user_salesforce_update(request):
     user = request.user
-    pending_verification = False
     salesforce_faculty_verified_failed = False
     salesforce_verification_pending_failed = False
 
