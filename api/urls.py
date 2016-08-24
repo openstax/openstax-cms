948b692acbbc811ecd59ac84ee5d3900cc76688d
Michael Harrison <mwharrison@rice.edu>
2016-08-24 10:00:42 -0500

    return object for user api, not list

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .views import AdopterViewSet, ImageViewSet, UserView, user_salesforce_update
+from .views import AdopterViewSet, ImageViewSet, user_salesforce_update, user_api
 
 router = routers.DefaultRouter()
 router.register(r'v0/images', ImageViewSet)
-router.register(r'user', UserView, 'user')
 router.register(r'adopters', AdopterViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^user_salesforce/$', user_salesforce_update, name='user_salesforce_update'),
+    url(r'^user_salesforce/$', user_salesforce_update, name='user_salesforce'),
+    url(r'^user/$', user_api, name='user_api'),
 ]
 
