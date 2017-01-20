6b8cbc417d49677329cc81d60f1870014e83ad67
Michael Harrison <mwharrison@rice.edu>
2017-01-20 15:44:09 -0600

    post logout redirect using next variable

--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,18 @@ from django.conf import settings
 def logout(request):
     auth_logout(request)
     return redirect(settings.OPENSTAX_ACCOUNTS_LOGOUT_URL)
+
+
+def post_logout(request):
+    host = request.get_host()
+    next = request.GET.get('next', '')
+
+    # if next param and host matches next location, redirect to it
+    # this should allow for relative or full urls in the next param
+    if next:
+        if host in next:
+            return redirect(next)
+        else:
+            return redirect('/')
+    else:
+        return redirect('/')
