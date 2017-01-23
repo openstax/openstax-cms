d761c6478a0d11fad72c8bf7220b81bcdea4bd3f
Michael Harrison <mwharrison@rice.edu>
2017-01-23 16:36:52 -0600

    add API endpoint for errata list

--- errata/urls.py
+++ errata/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'^$', views.news_index),
+    url(r'^$', views.errata_list),
     url(r'^(?P<id>\d+)/$', views.errata_detail),
 ]
