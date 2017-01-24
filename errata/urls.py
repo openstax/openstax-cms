bf70463b3a0130f7649592245ceb9a3b51646fab
Michael Harrison <mwharrison@rice.edu>
2017-01-24 16:42:25 -0600

    add pagination to errata list

--- errata/urls.py
+++ errata/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework import routers
 from . import views
 
+router = routers.SimpleRouter()
+router.register(r'', views.ErratumView, base_name='erratum')
+
 urlpatterns = [
-    url(r'^$', views.errata_list),
     url(r'^(?P<id>\d+)/$', views.errata_detail),
-]
+] + router.urls
