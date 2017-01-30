02557dc8dfa68846b788a275a8bde7acb8df88e6
Michael Harrison <mwharrison@rice.edu>
2017-01-30 15:50:57 -0600

    errata book relationship in post request

--- errata/serializers.py
+++ errata/serializers.py
@@ -1,10 +1,11 @@
 from .models import Errata
+from books.models import Book
 
 from rest_framework import serializers
 
 
 class ErrataSerializer(serializers.ModelSerializer):
-    book = serializers.StringRelatedField()
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
     error_type = serializers.StringRelatedField()
     resource = serializers.StringRelatedField(many=True)
 
