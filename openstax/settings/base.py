02cecdb002f5f5a49a822ba7c26fc300fd712aed
Michael Harrison <mwharrison@rice.edu>
2017-02-07 15:34:44 -0600

    remove pagination from rest framework

--- openstax/settings/base.py
+++ openstax/settings/base.py
@@ -206,8 +206,6 @@ COMPRESS_PRECOMPILERS = (
 
 #django rest framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
