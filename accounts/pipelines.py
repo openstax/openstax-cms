7e8bee797c6dae1edd37672821329ae1d9bcb026
Michael Harrison <mwharrison@rice.edu>
2017-02-20 14:42:56 -0600

    Add user to the student group if self_reported_role==student

--- accounts/pipelines.py
+++ accounts/pipelines.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import Group
+
+
 def save_profile(user, *args, **kwargs):
     # for now - setting email address to prevent issues, should update it
     # eventually
@@ -14,3 +17,11 @@ def update_email(user, response, *args, **kwargs):
 
     user.email = newest_email['value']
     user.save()
+
+
+def update_role(user, response, *args, **kwargs):
+    self_reported_role = response.get('self_reported_role')
+
+    if self_reported_role == 'student':
+        group, created = Group.objects.get_or_create(name=self_reported_role.title())
+        group.user_set.add(user)
