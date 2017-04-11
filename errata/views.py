4e6d10eeac1e8975780299f8481cdf12cf59118c
Michael Harrison <mwharrison@rice.edu>
2017-04-11 11:46:53 -0500

    do not display archived errata reports in api

--- errata/views.py
+++ errata/views.py
@@ -29,7 +29,7 @@ class ErrataFilter(FilterSet):
 
 
 class ErrataView(ModelViewSet):
-    queryset = Errata.objects.all()
+    queryset = Errata.objects.filter(archived=False)
     serializer_class = ErrataSerializer
     http_method_names = ['get', 'post', 'head']
     filter_backends = (DjangoFilterBackend, OrderingFilter)
