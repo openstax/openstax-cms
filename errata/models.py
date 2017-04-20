828e6ad3de5742eae90477800f65a9089ad3cc26
Michael Harrison <mwharrison@rice.edu>
2017-04-20 10:43:25 -0500

    do not override resolution notes if errata is completed/approved

--- errata/models.py
+++ errata/models.py
@@ -148,7 +148,7 @@ class Errata(models.Model):
             self.resolution_notes = "Our reviewers determined the textbook meets scope, sequence, and accuracy requirements as is.  No change will be made."
         if self.resolution == "Major Book Revision" and not self.resolution_notes:
             self.resolution_notes = "Our reviewers determined this would require a significant book revision.  While we cannot make this change at this time, we will consider it for future editions of this book."
-        if (self.status == "Reviewed" or self.status == "Completed") and self.resolution == "Approved":
+        if (self.status == "Reviewed" or self.status == "Completed") and self.resolution == "Approved" and not self.resolution_notes:
             self.resolution_notes = "Our reviewers accepted this change."
 
         super(Errata, self).save(*args, **kwargs)
