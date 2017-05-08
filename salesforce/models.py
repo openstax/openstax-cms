c7eb13f197f13163f786267329cbfbea3b84c50a
Michael Harrison <mwharrison@rice.edu>
2017-05-08 12:43:46 -0500

    add management command to update schools from salesforce

--- salesforce/models.py
+++ salesforce/models.py
@@ -10,3 +10,9 @@ class Adopter(models.Model):
     def __str__(self):
         return self.name
 
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
