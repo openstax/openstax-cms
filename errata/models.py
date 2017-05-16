cc50cd8e8707c81cd11fb00e85af735001075c63
Michael Harrison <mwharrison@rice.edu>
2017-05-16 16:24:50 -0500

    fix errata emails

--- errata/models.py
+++ errata/models.py
@@ -178,7 +178,7 @@ def send_status_update_email(sender, instance, created, **kwargs):
             subject = "We received your submission"
             body = "Thanks for your help! Your errata submissions help keep OpenStax resources high quality and up to date."
             send_email = True
-        elif instance.status == 'Reviewed' or instance.status == 'Completed' and instance.resolution == 'Will Not Fix':
+        elif instance.status == 'Reviewed' and instance.resolution == 'Will Not Fix':
             subject = "We reviewed your erratum suggestion"
             body = "Thanks again for your submission. Our reviewers have evaluated it and have determined there will be no change made."
             send_email = True
@@ -186,7 +186,7 @@ def send_status_update_email(sender, instance, created, **kwargs):
             subject = "We received your submission"
             body = "Thanks again for your submission. Our reviewers have evaluated it and have determined that a change will be made. We will email you again when the appropriate resource has been updated."
             send_email = True
-        elif instance.status == 'Completed':
+        elif instance.status == 'Completed' and instance.resolution == 'Approved':
             subject = "Your correction is live"
             body = "The correction you suggested has been incorporated into the appropriate OpenStax resource. Thanks for your help!"
             send_email = True
