d8afbc68915a3cb6b51865745c5a6d68bf070088
Michael Harrison <mwharrison@rice.edu>
2017-11-06 13:34:00 -0600

    Errata admin js (#521)
    * Move book array into toggle function
    
    * Fix query.inArray

--- errata/static/errata-admin.js
+++ errata/static/errata-admin.js
@@ -3,9 +3,9 @@
         var bookField = $('#id_book'),
             is_assessment = $('#id_is_assessment_errata');
 
-        function toggleVerified(bookField) {
+        function toggleVerified(value) {
             var bookArray = [33, 32, 31, 47];
-            jQuery.inArray(bookField, bookArray) ? is_assessment.show() : is_assessment.hide();
+            $.inArray(value, bookArray) ? is_assessment.show() : is_assessment.hide();
         }
 
         // show/hide on load based on pervious value of selectField
