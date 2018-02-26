27ce650c68638a30423cf77ab685c40aabf81a8a
Michael Harrison <mwharrison@rice.edu>
2018-02-26 13:59:37 -0600

    Remove CacheControl

--- openstax/settings/qa.py
+++ openstax/settings/qa.py
@@ -8,9 +8,6 @@ DEFAULT_FILE_STORAGE = 'storages.S3Storage.S3Storage'
 AWS_STORAGE_BUCKET_NAME = 'openstax-assets'
 AWS_STORAGE_DIR = 'oscms-qa'
 AWS_S3_CUSTOM_DOMAIN = 'd3bxy9euw4e147.cloudfront.net'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=0',
-}
 # S3 static file storage using custom backend
 STATICFILES_LOCATION = '{}/static'.format(AWS_STORAGE_DIR)
 STATICFILES_STORAGE = 'openstax.custom_storages.StaticStorage'
