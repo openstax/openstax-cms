aa45a6f9aad854c9153825ddef6c6f0b083db469
Michael Harrison <mwharrison@rice.edu>
2018-02-26 16:08:03 -0600

    Update tests

--- pages/tests.py
+++ pages/tests.py
@@ -103,33 +103,30 @@ class AdminPages(TestCase, WagtailTestUtils):
     def target(self):
         def test_redirect(path):
             response = self.client.get(path)
-            self.assertEqual(response.status_code, 301)
-            perm_redirect_url = response.url
-            response = self.client.get(perm_redirect_url)
             self.assertEqual(response.status_code, 200)
             return response
         return test_redirect
 
     def test_admin_link(self):
-        self.target('/admin')
+        self.target('/admin/')
 
     def test_images_link(self):
-        self.target('/admin/images')
+        self.target('/admin/images/')
 
     def test_pages_link(self):
-        self.target('/admin/pages')
+        self.target('/admin/pages/')
 
     def test_documents_link(self):
-        self.target('/admin/documents')
+        self.target('/admin/documents/')
 
     def test_snippets_link(self):
-        self.target('/admin/snippets')
+        self.target('/admin/snippets/')
 
     def test_users_link(self):
-        self.target('/admin/users')
+        self.target('/admin/users/')
 
     def test_groups_link(self):
-        self.target('/admin/groups')
+        self.target('/admin/groups/')
 
     # A lazy test of our search field without parsing html
     def test_admin_search(self):
