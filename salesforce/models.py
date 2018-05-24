bca4001dc1cd1515ff11082e355ee1567d1333bb
Michael Harrison <mwharrison@rice.edu>
2018-05-24 13:14:39 -0500

    Add new fields for impact map to school API

--- salesforce/models.py
+++ salesforce/models.py
@@ -13,6 +13,29 @@ class Adopter(models.Model):
 
 class School(models.Model):
     name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    type = models.CharField(max_length=255, null=True, blank=True)
+    adoption_date = models.CharField(max_length=255, null=True, blank=True)
+    key_institutional_partner = models.BooleanField(default=False)
+    achieving_the_dream_school = models.BooleanField(default=False)
+    hbcu = models.BooleanField(default=False)
+    texas_higher_ed = models.BooleanField(default=False)
+    undergraduate_enrollment = models.CharField(max_length=255, null=True, blank=True)
+    pell_grant_recipients = models.CharField(max_length=255, null=True, blank=True)
+    percent_students_pell_grant = models.CharField(max_length=255, null=True, blank=True)
+    current_year_students = models.CharField(max_length=255, null=True, blank=True)
+    all_time_students = models.CharField(max_length=255, null=True, blank=True)
+    current_year_savings = models.CharField(max_length=255, null=True, blank=True)
+    all_time_savings = models.CharField(max_length=255, null=True, blank=True)
+    physical_country = models.CharField(max_length=255, null=True, blank=True)
+    physical_street = models.CharField(max_length=255, null=True, blank=True)
+    physical_city = models.CharField(max_length=255, null=True, blank=True)
+    physical_state_province = models.CharField(max_length=255, null=True, blank=True)
+    physical_zip_postal_code = models.CharField(max_length=255, null=True, blank=True)
+    long = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    lat = models.DecimalField(max_digits=8, decimal_places=3, null=True, blank=True)
+    testimonial = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- salesforce/serializers.py
+++ salesforce/serializers.py
@@ -8,4 +8,27 @@ class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = ('id',
-                  'name')
+                  'name',
+                  'phone',
+                  'website',
+                  'type',
+                  'adoption_date',
+                  'key_institutional_partner',
+                  'achieving_the_dream_school',
+                  'hbcu',
+                  'texas_higher_ed',
+                  'undergraduate_enrollment',
+                  'pell_grant_recipients',
+                  'percent_students_pell_grant',
+                  'current_year_students',
+                  'all_time_students',
+                  'current_year_savings',
+                  'all_time_savings',
+                  'physical_country',
+                  'physical_street',
+                  'physical_city',
+                  'physical_state_province',
+                  'physical_zip_postal_code',
+                  'long',
+                  'lat',
+                  'testimonial',)
