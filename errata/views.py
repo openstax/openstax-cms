402d8a21eb331a129db2160345f2d734b76c08e0
Michael Harrison <mwharrison@rice.edu>
2018-06-11 15:06:41 -0500

    Exclude assessment errata instead of filtering on 'No'

--- errata/views.py
+++ errata/views.py
@@ -29,7 +29,7 @@ class ErrataFilter(FilterSet):
 
 
 class ErrataView(ModelViewSet):
-    queryset = Errata.objects.filter(archived=False).filter(is_assessment_errata='No')
+    queryset = Errata.objects.filter(archived=False).exclude(is_assessment_errata='Yes')
     serializer_class = ErrataSerializer
     http_method_names = ['get', 'post', 'head']
     filter_backends = (DjangoFilterBackend, OrderingFilter)
