d901efecfbf2bb9f85540a0eff17fd47217d6053
Michael Harrison <mwharrison@rice.edu>
2018-08-06 11:10:33 -0500

    Add subjects API & template cleanup

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
-from .models import Role
-from .serializers import RoleSerializer
+from .models import Role, Subject
+from .serializers import RoleSerializer, SubjectSerializer
 
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
+
+class SubjectsViewSet(viewsets.ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
