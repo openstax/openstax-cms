2377647793f724dff4539289d0f32e66fc068067
Michael Harrison <mwharrison@rice.edu>
2018-08-28 13:15:42 -0500

    Add model to store Salesforce connection information

--- salesforce/admin.py
+++ salesforce/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 
-from .models import School, MapBoxDataset
+from .models import School, MapBoxDataset, SalesforceSettings
 
 
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone']
     list_filter = ('key_institutional_partner', 'achieving_the_dream_school', 'hbcu', 'texas_higher_ed')
 
+
+class SalesforceSettingsAdmin(admin.ModelAdmin):
+    # only allow one SF Setting to exist
+    def has_add_permission(self, request):
+        num_objects = self.model.objects.count()
+        if num_objects >= 1:
+            return False
+        else:
+            return True
+
+
+admin.site.register(SalesforceSettings, SalesforceSettingsAdmin)
 admin.site.register(School, SchoolAdmin)
 admin.site.register(MapBoxDataset)
