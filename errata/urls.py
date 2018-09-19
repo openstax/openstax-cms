a4e91d9f6f1ea6c7547bbf14e10b9bdb642e6d1f
Michael Harrison <mwharrison@rice.edu>
2018-09-19 09:39:21 -0500

    Fix Errata URLs

--- errata/urls.py
+++ errata/urls.py
@@ -6,8 +6,8 @@ router = routers.SimpleRouter()
 router.register(r'', views.ErrataView)
 
 urlpatterns = [
-    url(r'^/admin/dashboard/$', views.dashboard),
-    url(r'^/admin/list/$', views.list),
-    url(r'^/admin/edit/$', views.edit),
+    url(r'^admin/dashboard/$', views.dashboard),
+    url(r'^admin/list/$', views.list),
+    url(r'^admin/edit/$', views.edit),
     url(r'^', include(router.urls)),
 ]
