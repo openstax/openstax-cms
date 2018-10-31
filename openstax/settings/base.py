0cfea32f27c6b11719049dd2ac32dc8cc0d35f54
Michael Harrison <mwharrison@rice.edu>
2018-10-31 09:19:24 -0500

    Add ID to Salesforce geoJSON export (#676)
    * update Salesforce settings
    
    * use Salesforce settings from database
    
    * import django setup
    
    * revert SF settings change

--- openstax/settings/base.py
+++ openstax/settings/base.py
@@ -20,7 +20,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'openstax',
+        'NAME': 'oscms_prodcms',
     }
 }
 
