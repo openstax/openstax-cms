57e9cd8f887fee0f71f7985fd205aad90ede9753
Michael Harrison <mwharrison@rice.edu>
2019-03-19 12:40:08 -0500

    add oxauth logout function

--- oxauth/urls.py
+++ oxauth/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import login, get_user_data
+from .views import login, logout, get_user_data
 
 urlpatterns = [
     url(r'login', login, name='login'),
+    url(r'login', logout, name='logout'),
     url(r'user', get_user_data, name='user'),
 ]
