970cf88ddad1fb5d760382b3a78cffb660362b2b
Michael Harrison <mwharrison@rice.edu>
2019-04-17 07:08:13 -0500

    Add cache control and last modified headers to the page API

--- openstax/api.py
+++ openstax/api.py
@@ -2,6 +2,7 @@ from wagtail.api.v2.endpoints import PagesAPIEndpoint
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.images.api.v2.endpoints import ImagesAPIEndpoint
 from wagtail.documents.api.v2.endpoints import DocumentsAPIEndpoint
+from wagtail.core.models import Page
 
 # Create the router. "wagtailapi" is the URL namespace
 api_router = WagtailAPIRouter('wagtailapi')
@@ -27,10 +28,22 @@ class DocumentsAPIEndpoint(DocumentsAPIEndpoint):
         'file',
     ]
 
+
+class CachedPagesAPIEndpoint(PagesAPIEndpoint):
+    def detail_view(self, request, pk):
+        response = super().detail_view(request, pk)
+        response['Cache-Control'] = 'max-age=290304000, public'
+
+        page = Page.objects.get(pk=pk)
+        response['Last-Modified'] = page.last_published_at
+
+        return response
+
+
 # Add the three endpoints using the "register_endpoint" method.
 # The first parameter is the name of the endpoint (eg. pages, images). This
 # is used in the URL of the endpoint
 # The second parameter is the endpoint class that handles the requests
-api_router.register_endpoint('pages', PagesAPIEndpoint)
+api_router.register_endpoint('pages', CachedPagesAPIEndpoint)
 api_router.register_endpoint('images', ImagesAPIEndpoint)
 api_router.register_endpoint('documents', DocumentsAPIEndpoint)
