e9b1d1db93932685094ba863f46a7aa94acf19d9
Michael Harrison <mwharrison@rice.edu>
2019-04-23 10:07:10 -0500

    Update display names for text color

--- books/models.py
+++ books/models.py
@@ -355,20 +355,20 @@ GREY_WHITE = 'grey_white'
 WHITE_GREY = 'white_grey'
 WHITE_ORANGE = 'white_orange'
 BOOK_COVER_TEXT_COLOR = (
-    (YELLOW, 'yellow'),
-    (LIGHT_BLUE, 'light_blue'),
-    (DARK_BLUE, 'dark_blue'),
-    (GREEN, 'green'),
-    (WHITE, 'white'),
-    (GREY, 'grey'),
-    (RED, 'red'),
-    (WHITE_RED, 'white_red'),
-    (WHITE_BLUE, 'white_blue'),
-    (GREEN_WHITE, 'green_white'),
-    (YELLOW_WHITE, 'yellow_white'),
-    (GREY_WHITE, 'grey_white'),
-    (WHITE_GREY, 'white_grey'),
-    (WHITE_ORANGE, 'white_orange'),
+    (YELLOW, 'Yellow'),
+    (LIGHT_BLUE, 'Light Blue'),
+    (DARK_BLUE, 'Dark Blue'),
+    (GREEN, 'Green'),
+    (WHITE, 'White'),
+    (GREY, 'Grey'),
+    (RED, 'Red'),
+    (WHITE_RED, 'White/Red'),
+    (WHITE_BLUE, 'White/Blue'),
+    (GREEN_WHITE, 'Green/White'),
+    (YELLOW_WHITE, 'Yellow/White'),
+    (GREY_WHITE, 'Grey/White'),
+    (WHITE_GREY, 'White/Grey'),
+    (WHITE_ORANGE, 'White/Orange'),
 )
 
 LIVE = 'live'
