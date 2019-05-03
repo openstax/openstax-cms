76e45cccf17137c90da0c70062cc11eb5679c276
Michael Harrison <mwharrison@rice.edu>
2019-05-03 12:46:45 -0500

    add slash to oxauth social login urls (#766)

--- oxauth/urls.py
+++ oxauth/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from .views import login, logout, get_user_data
 
 urlpatterns = [
-    url(r'', include('social_django.urls', namespace='social')),
+    url(r'/', include('social_django.urls', namespace='social')),
     url(r"^/login/?$", login, name="login"),
     url(r"^/logout/?$", logout, name="logout"),
     url(r"^/user/?$", get_user_data, name="user"),
