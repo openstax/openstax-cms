8afd369129251dbe52b658688a0280014871c16e
Michael Harrison <mwharrison@rice.edu>
2019-05-08 07:49:56 -0500

    Add tests for the document API

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from wagtail.tests.utils import WagtailTestUtils
 from wagtail.images.tests.utils import Image, get_test_image_file
-
+from wagtail.documents.models import Document
 
 class ImageAPI(TestCase, WagtailTestUtils):
 
@@ -37,3 +37,42 @@ class ImageAPI(TestCase, WagtailTestUtils):
         self.assertEqual(response_dict['meta']['total_count'], 1)
         returned_title = response_dict['items'][0]['title']
         self.assertEqual(expected_title, returned_title)
+
+
+class DocumentAPI(TestCase, WagtailTestUtils):
+    def setUp(self):
+        self.login()
+
+    def test_api_v2_single_document(self):
+        response = self.client.get('/api/v2/documents/')
+        self.assertEqual(response.status_code, 200)
+        response_dict = eval(response.content.decode(response.charset))
+        self.assertIsInstance(response_dict, dict)
+        self.assertEqual(response_dict['meta']['total_count'], 0)
+        self.assertEqual(response_dict['items'], [])
+
+        expected_title = "Test document"
+        image = Document.objects.create(
+            title=expected_title,
+            file=get_test_image_file(),
+        )
+
+        response = self.client.get('/api/v2/documents/')
+        self.assertEqual(response.status_code, 200)
+        response_dict = eval(response.content.decode(response.charset))
+        self.assertIsInstance(response_dict, dict)
+        self.assertEqual(response_dict['meta']['total_count'], 1)
+        returned_title = response_dict['items'][0]['title']
+        self.assertEqual(expected_title, returned_title)
+
+    def test_can_search_documents(self):
+        image = Document.objects.create(
+            title="OpenStax",
+            file=get_test_image_file(),
+        )
+
+        response = self.client.get('/api/v2/documents/?search=OpenStax')
+        self.assertEqual(response.status_code, 200)
+        response_dict = eval(response.content.decode(response.charset))
+        self.assertIsInstance(response_dict, dict)
+        self.assertEqual(response_dict['meta']['total_count'], 1)
