fd8c82855f233d2bc7fba482191de46ab5afef5a
Berk Alp Yakici <berkalp.y@gmail.com>
2019-06-14 10:34:13 -0500

    Add tests for wagtailimportexport forms.

--- wagtailimportexport/tests/test_views.py
+++ wagtailimportexport/tests/test_views.py
@@ -4,8 +4,9 @@ import tempfile
 import zipfile
 from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, Client 
 from wagtailimportexport.compat import Page
+from django.urls import reverse
 from wagtailimportexport import views  # read this aloud
 
 class TestViews(TestCase):
@@ -31,3 +32,23 @@ class TestViews(TestCase):
 
         assert allpages['pages'][0]['content']['test'][0]['pk'] == None
         assert allpages['pages'][0]['content']['test'][0]['haha'] == 'yup'
+
+class TestForms(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_importfile(self):
+        response = self.client.get(reverse('wagtailimportexport_admin:import_from_file'))
+        self.assertNotEqual(response.status_code, 404)
+    
+    def test_exportfile(self):
+        response = self.client.get(reverse('wagtailimportexport_admin:export_to_file'))
+        self.assertNotEqual(response.status_code, 404)
+
+    def test_duplicate(self):
+        response = self.client.get(reverse('wagtailimportexport_admin:duplicate', args=[1]))
+        self.assertNotEqual(response.status_code, 404)
+
+    def test_index(self):
+        response = self.client.get(reverse('wagtailimportexport_admin:index'))
+        self.assertNotEqual(response.status_code, 404)
