d644a28c9f6966a372e7e2fb24b3b0b0128e98c7
Michael Harrison <mwharrison@rice.edu>
2019-06-24 10:22:19 -0500

    Add image renditions to blog pages (#799)
    * Add image renditions to blog pages
    
    * add some news tests
    
    * update rendition sizes
    
    * remove print statement

--- news/tests.py
+++ news/tests.py
@@ -1,4 +1,7 @@
+from django.utils import timezone
 from wagtail.tests.utils import WagtailPageTests
+from wagtail.core.models import Page
+from pages.models import HomePage
 from shared.test_utilities import assertPathDoesNotRedirectToTrailingSlash
 from unittest.mock import MagicMock
 from news.models import NewsIndex, NewsArticle, PressIndex, PressRelease
@@ -6,6 +9,34 @@ from news.models import NewsIndex, NewsArticle, PressIndex, PressRelease
 class NewsTests(WagtailPageTests):
     def setUp(self):
         pass
+
+    @classmethod
+    def setUpTestData(cls):
+        # create root page
+        root_page = Page.objects.get(title="Root")
+        # create homepage
+        homepage = HomePage(title="Hello World",
+                            slug="hello-world",
+                            )
+        # add homepage to root page
+        root_page.add_child(instance=homepage)
+        # create book index page
+        news_index = NewsIndex(title="News Index")
+        # add book index to homepage
+        homepage.add_child(instance=news_index)
+
+        cls.news_index = Page.objects.get(id=news_index.id)
+
+    def test_can_create_news_article(self):
+        news_index = NewsIndex.objects.all()[0]
+        article = NewsArticle(title="Article 1",
+                              slug="article",
+                              date=timezone.now(),
+                              heading="Sample Article",
+                              subheading="Sample Subheading",
+                              author="OpenStax")
+        news_index.add_child(instance=article)
+        self.assertEqual(article.heading, "Sample Article")
 
     def test_slashless_apis_are_good(self):
         NewsIndex.objects.all = MagicMock(return_value=MagicMock(pk=3))
