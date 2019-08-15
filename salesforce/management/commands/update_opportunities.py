9465502b4b78e10ae12a433342679fd525283443
Michael Harrison <mwharrison@rice.edu>
2019-08-15 16:10:44 -0500

    Adoption api (#840)
    * add search to school admin
    
    * add API for adoption renewal info from Salesforce
    
    I'm sure there is going to be some back-and-forth on this - but getting the MVP up so we can start iterating.

---
+++ salesforce/management/commands/update_opportunities.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from salesforce.models import AdoptionOpportunityRecord
+from salesforce.salesforce import Salesforce
+
+
+class Command(BaseCommand):
+    help = "update book adoptions from salesforce.com for use on the renewal form"
+
+    def handle(self, *args, **options):
+        with Salesforce() as sf:
+            command = "SELECT Id, OS_Accounts_ID__c, Book_Text__c, Contact_Email__c, School_Name__c, Yearly_Students__c from Opportunity"
+            response = sf.query_all(command)
+            records = response['records']
+            num_created = 0
+            for record in records:
+
+                opportunity, created = AdoptionOpportunityRecord.objects.update_or_create(
+                    opportunity_id=record['Id'],
+                    defaults={'account_id': record['OS_Accounts_ID__c'],
+                              'book_name': record['Book_Text__c'],
+                              'email': record['Contact_Email__c'],
+                              'school': record['School_Name__c'],
+                              'yearly_students': record['Yearly_Students__c']
+                              },
+                )
+
+                if created:
+                    num_created = num_created + 1
+
+                opportunity.save()
+
+            response = self.style.SUCCESS("Successfully updated opportunity records. {} were newly created.".format(num_created))
+        self.stdout.write(response)
