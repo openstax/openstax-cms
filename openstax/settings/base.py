e179c10e49facd54f134db0256293a6c1a8e40b0
Ed Woodward <ecw1@rice.edu>
2019-09-20 09:14:56 -0500

    Fixed database name

--- openstax/settings/base.py
+++ openstax/settings/base.py
@@ -36,7 +36,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'openstax',
+        'NAME': 'oscms_prodcms',
     }
 }
 
