3052ef6cca35934b4dd896ee67c37580eef498db
Colby <colby.powers27@gmail.com>
2019-11-15 12:41:28 -0600

    Update admin.py
    Adding user_name to superuser/CM view on errata edit page. I also removed the user_email view from the else: statement.

--- errata/admin.py
+++ errata/admin.py
@@ -144,6 +144,7 @@ class ErrataAdmin(ExportActionModelAdmin):
                            'error_type',
                            'number_of_errors',
                            'resource',
+                           'user_name',
                            'user_email',
                            'accounts_link',
                            'file_1',
@@ -151,6 +152,7 @@ class ErrataAdmin(ExportActionModelAdmin):
             self.readonly_fields = ['id',
                                     'created',
                                     'modified',
+                                    'user_name',
                                     'user_email',
                                     'accounts_link']
             self.save_as = True
@@ -200,7 +202,6 @@ class ErrataAdmin(ExportActionModelAdmin):
                            'error_type',
                            'number_of_errors',
                            'resource',
-                           'user_email',
                            'accounts_link',
                            'file_1',
                            'file_2']
@@ -222,7 +223,6 @@ class ErrataAdmin(ExportActionModelAdmin):
                                     'error_type',
                                     'number_of_errors',
                                     'resource',
-                                    'user_email',
                                     'accounts_link',
                                     'file_1',
                                     'file_2']
