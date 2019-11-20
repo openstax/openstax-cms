d49c152a719e441533423990730abd7494fbbfc8
Michael Harrison <mwharrison@rice.edu>
2019-11-20 07:28:11 -0600

    Revert "revert wagtail admin api override (fixed pages drawer menu) (#880)"
    This reverts commit 45dd7dcc48e9a92f98dd7cb947837a5c70602463.

--- openstax/settings/base.py
+++ openstax/settings/base.py
@@ -238,7 +238,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
