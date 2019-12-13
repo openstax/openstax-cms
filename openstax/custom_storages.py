968640f6afda0b459b8831ad25a4d2670d80fad2
Ed Woodward <ecw1@rice.edu>
2019-12-13 10:23:31 -0600

    Upgrade to Wagtail 2.7 and Django 2.8 (#927)
    * Upgrade to Wagtail 2.7 and Django 2.8
    
    * Removed Manifest File Storeage. It is not needed with Wagtail 2.7

--- openstax/custom_storages.py
+++ openstax/custom_storages.py
@@ -1,15 +1,10 @@
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
-from django.contrib.staticfiles.storage import ManifestFilesMixin
 from whitenoise.storage import CompressedManifestStaticFilesStorage
 
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
-
-
-class ManifestS3Storage(CompressedManifestStaticFilesStorage, StaticStorage):
-    pass
 
 
 class MediaStorage(S3BotoStorage):
