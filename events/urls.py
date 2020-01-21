dc1cf7670ff3d9a2594db3004e2bb0016e1308c1
Michael Harrison <mwharrison@rice.edu>
2020-01-21 09:53:56 -0600

    add email check to eventbrite api check

--- events/urls.py
+++ events/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .views import SessionViewSet, eventbrite
+from .views import SessionViewSet, check_reg_status
 
 router = routers.DefaultRouter()
 router.register(r'sessions', SessionViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^eventbrite/$', eventbrite, name='eventbrite'),
+    url(r'^check/$', check_reg_status, name='check_reg_status'),
 ]
 
--- events/views.py
+++ events/views.py
@@ -1,3 +1,6 @@
+import json
+import requests
+from pprint import pprint
 from eventbrite import Eventbrite
 
 from django.shortcuts import render
@@ -9,16 +12,18 @@ from rest_framework import viewsets
 from .models import Event, Session
 from .serializers import SessionSerializer
 
-def eventbrite(request):
+def check_reg_status(request):
     eventbrite = Eventbrite(settings.EVENTBRITE_API_PRIVATE_TOKEN)
     event_id = Event.objects.all()[0].eventbrite_event_id
-    event = eventbrite.get_event(event_id)
     attendees = eventbrite.get_event_attendees(event_id=event_id)
 
+    email = request.GET.get('email', None)
 
+    attendee_emails = []
+    for attendee in attendees['attendees']:
+        attendee_emails.append(attendee['profile']['email'])
 
-
-    return JsonResponse(attendee)
+    return JsonResponse({'registered': email in attendee_emails})
 
 
 class SessionViewSet(viewsets.ModelViewSet):
