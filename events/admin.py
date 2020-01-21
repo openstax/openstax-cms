6f9a88cc6b418d02f5543281b693524def3fc36b
Michael Harrison <mwharrison@rice.edu>
2020-01-21 13:24:26 -0600

    add tests for eventbrite checks

--- events/admin.py
+++ events/admin.py
@@ -2,15 +2,7 @@ from django.contrib import admin
 
 from .models import Event, Session, Registration
 
-# class SessionAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'phone']
-#     list_filter = ('key_institutional_partner', 'achieving_the_dream_school', 'hbcu', 'texas_higher_ed')
-#     search_fields = ['name', ]
-#
-#     def has_add_permission(self, request):
-#         return False
 
-#admin.site.register(Partner, PartnerAdmin)
 admin.site.register(Event)
 admin.site.register(Session)
 admin.site.register(Registration)
--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from events.models import Event
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+class EventBriteTest(APITestCase, TestCase):
+    client = APIClient()
+
+    def setUp(self):
+        event = Event.objects.create(eventbrite_event_id='89893504893')
+
+    def test_can_check_registration_by_email(self):
+        response = self.client.post('/apps/cms/api/events/check?email=mwharrison@rice.edu', follow=True) # this needs the test event in eventbrite to properly test
+        self.assertEqual(response.content.decode("utf-8"), '{"registered": true}')
+
+    def test_returns_false_if_not_registered(self):
+        response = self.client.post('/apps/cms/api/events/check?email=not_registered@openstax.org', follow=True)  # this needs the test event in eventbrite to properly test
+        self.assertEqual(response.content.decode("utf-8"), '{"registered": false}')
