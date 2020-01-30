05a72dd25ab1166d20b303300663557686f2cc10
Michael Harrison <mwharrison@rice.edu>
2020-01-30 09:58:41 -0600

    equals -> contains

--- events/tests.py
+++ events/tests.py
@@ -11,11 +11,11 @@ class EventBriteTest(APITestCase, TestCase):
 
     def test_can_check_registration_by_email(self):
         response = self.client.post('/apps/cms/api/events/check?email=mwharrison@rice.edu', follow=True)
-        self.assertEqual(response.content.decode("utf-8"), '{"eventbrite_registered": true, "session_registered": false}')
+        self.assertContains(response, '"eventbrite_registered": true')
 
     def test_returns_false_if_not_registered(self):
         response = self.client.post('/apps/cms/api/events/check?email=not_registered@openstax.org', follow=True)
-        self.assertEqual(response.content.decode("utf-8"), '{"eventbrite_registered": false, "session_registered": false}')
+        self.assertContains(response, '"eventbrite_registered": false')
 
 
 class SessionTest(APITestCase, TestCase):
