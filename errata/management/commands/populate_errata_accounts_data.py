434821981db7d62e941beeb779f0c671d13c0fe2
Michael Harrison <mwharrison@rice.edu>
2020-01-31 10:48:46 -0600

    actually save errata during management command run (#953)

--- errata/management/commands/populate_errata_accounts_data.py
+++ errata/management/commands/populate_errata_accounts_data.py
@@ -8,6 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Total Errata: {}'.format(Errata.objects.all().count()))
         updated_errata = 0
+        no_user = 0
+
         for errata in Errata.objects.all():
             try:
                 user = get_user_info(errata.submitted_by_account_id)
@@ -15,9 +17,13 @@ class Command(BaseCommand):
                     errata.accounts_user_email = user['email']
                     errata.accounts_user_name = user['fullname']
                     errata.accounts_user_faculty_status = user['faculty_status']
+                    errata.save()
                     updated_errata = updated_errata + 1
+                else:
+                    no_user = no_user + 1
             except Exception as e:
                 print(e)
                 break
 
-        print('Updated Errata: {}'.format(updated_errata))
+        print('Updated Errata Count: {}'.format(updated_errata))
+        print('No User Found Count: {}'.format(no_user))
