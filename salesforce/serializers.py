8b58593392447b4652f2fa211cc815e97ae0e934
Michael Harrison <mwharrison@rice.edu>
2020-03-06 10:51:26 -0600

    reverse formstack hiding check

--- salesforce/serializers.py
+++ salesforce/serializers.py
@@ -61,8 +61,8 @@ class PartnerSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ret = super().to_representation(instance)
 
-        # if lead sharing is checked in salesforce, we hide the formstack_url (which hides request info button on FE)
-        if ret['lead_sharing']:
+        # if lead sharing is unchecked in salesforce, we hide the formstack_url (which hides request info button on FE)
+        if not ret['lead_sharing']:
             ret['formstack_url'] = False
 
         # Here we filter the null values and creates a new dictionary
