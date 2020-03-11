13f68eafe3ec65ab1cd8d579d95a492e0c11734c
Michael Harrison <mwharrison@rice.edu>
2020-03-11 09:58:51 -0500

    Add ability to hide filters from partners page (#990)

--- salesforce/models.py
+++ salesforce/models.py
@@ -209,6 +209,7 @@ class Partner(models.Model):
 class PartnerFieldNameMapping(models.Model):
     salesforce_name = models.CharField(max_length=255)
     display_name = models.CharField(max_length=255)
+    hidden = models.BooleanField(default=False)
 
     def __str__(self):
         return self.display_name
