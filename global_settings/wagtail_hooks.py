b28ca4abf8a6986b96bfb89cf8737c8f737fee4e
Michael Harrison <mwharrison@rice.edu>
2020-03-27 16:33:48 -0500

    update boto import to use boto3 (#1000)

--- global_settings/wagtail_hooks.py
+++ global_settings/wagtail_hooks.py
@@ -1,4 +1,4 @@
-import boto
+import boto3
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
 from wagtail.core import hooks
