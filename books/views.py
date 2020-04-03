8533ccc26fbc75c6b4b7b351e89db4687a8a0c4d
Michael Harrison <mwharrison@rice.edu>
2020-04-03 16:16:52 -0500

    Featured resources (#1004)
    * Add featured resources and header text
    
    * remove book cache busting query string
    
    * fix migrations to match production

--- books/views.py
+++ books/views.py
@@ -8,13 +8,13 @@ from .models import BookIndex, Book
 @csrf_exempt
 def book_index(request):
     page = BookIndex.objects.all()[0]
-    return redirect('/apps/cms/api/v2/pages/{}/?cache={}'.format(page.pk, randrange(100000)))
+    return redirect('/apps/cms/api/v2/pages/{}/'.format(page.pk))
 
 
 @csrf_exempt
 def book_detail(request, slug):
     try:
         page = Book.objects.get(slug=slug)
-        return redirect('/apps/cms/api/v2/pages/{}/?cache={}'.format(page.pk, randrange(100000)))
+        return redirect('/apps/cms/api/v2/pages/{}/'.format(page.pk))
     except Book.DoesNotExist:
         raise Http404("Book does not exist.")
