389cdb94b2f0ac6ddfbf04b20b5a885fad79c1a8
Michael Harrison <mwharrison@rice.edu>
2020-04-22 12:28:17 -0500

    catch index errors on cache invalidation

--- global_settings/functions.py
+++ global_settings/functions.py
@@ -21,5 +21,7 @@ def invalidate_cloudfront_caches():
         )
     except CloudfrontDistribution.DoesNotExist:
         return
+    except IndexError:
+        return
     except NoCredentialsError:
         print('No AWS credentials set - unable to invalidate cache')
