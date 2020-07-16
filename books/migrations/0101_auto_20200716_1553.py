928b96c4b56c44a6984530ef8b0c848f3bb1b262
Michael Harrison <mwharrison@rice.edu>
2020-07-16 17:09:41 -0500

    Revert "fix book migration"
    This reverts commit f71212dd7753d13f106107d55f12362a07baf4d9.

--- books/migrations/0101_auto_20200716_1553.py
+++ books/migrations/0101_auto_20200716_1553.py
@@ -11,7 +11,6 @@ class Migration(migrations.Migration):
         ('snippets', '0022_subject_page_content'),
         ('wagtaildocs', '0010_document_file_hash'),
         ('wagtailcore', '0045_assign_unlock_grouppagepermission'),
-        ('books', '0053_auto_20180409_1242'),
         ('books', '0100_videofacultyresources'),
     ]
 
