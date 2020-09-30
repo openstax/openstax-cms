6ad20a2b977476697661246e62d722fb411d9027
Michael Harrison <mwharrison@rice.edu>
2020-09-30 13:17:27 -0500

    remove duplicate roleviewset

--- snippets/views.py
+++ snippets/views.py
@@ -21,8 +21,3 @@ class SubjectList(viewsets.ReadOnlyModelViewSet):
         if name is not None:
             queryset = queryset.filter(name=name)
         return queryset
-
-
-class RoleViewSet(viewsets.ModelViewSet):
-    queryset = Role.objects.all()
-    serializer_class = RoleSerializer
