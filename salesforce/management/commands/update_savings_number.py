cd93b85d8b447c5ca330e9a474a58d95527f9ed5
Michael Harrison <mwharrison@rice.edu>
2020-10-21 11:48:38 -0500

    add adoption and savings numbers to books (#1072)

--- salesforce/management/commands/update_savings_number.py
+++ salesforce/management/commands/update_savings_number.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
+from books.models import Book
 from salesforce.models import SavingsNumber
 from salesforce.salesforce import Salesforce
 
 class Command(BaseCommand):
-    help = "update schools from salesforce.com"
+    help = "update savings numbers from Salesforce"
 
     def handle(self, *args, **options):
         with Salesforce() as sf:
@@ -17,13 +18,34 @@ class Command(BaseCommand):
             adoption_number = int(response['records'][0]['expr0'])
             sf_data.adoptions_count = adoption_number
 
-
             savings_query = "Select SUM(All_Time_Savings2__c) FROM Account"
             response = sf.query_all(savings_query)
-            savings_number = float(response['records'][0]['expr0'])
+            savings_number = int(response['records'][0]['expr0'])
             sf_data.savings = savings_number
 
-
             sf_data.save()
+
+
+            # Book specific updates
+            books = Book.objects.all()
+            adoption_query = "SELECT Book__r.Name, Count(Id) FROM Adoption__c GROUP BY Book__r.Name"
+            adoption_response = sf.query_all(adoption_query)
+
+            savings_query = "SELECT Book__r.Name, SUM(Yearly_Savings__c) FROM Opportunity WHERE IsWon=True GROUP BY Book__r.Name"
+            savings_response = sf.query_all(savings_query)
+
+
+            for book in books:
+                for record in adoption_response['records']:
+                    if record['Name'] == book.salesforce_name:
+                        book.adoptions = int(record['expr0'])
+
+                for record in savings_response['records']:
+                    if record['Name'] == book.salesforce_name:
+                        book.savings = int(record['expr0'])
+
+                book.save()
+
+
             response = self.style.SUCCESS("Updating savings numbers complete!")
         self.stdout.write(response)
