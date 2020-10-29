5ba36385ce49d8de5bf1d5ce511ee87e268248e4
Michael Harrison <mwharrison@rice.edu>
2020-10-29 12:41:58 -0500

    remove unused import

--- salesforce/management/commands/sync_reviews.py
+++ salesforce/management/commands/sync_reviews.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from salesforce.models import PartnerReview, Partner
+from salesforce.models import PartnerReview
 from salesforce.salesforce import Salesforce
 
 
