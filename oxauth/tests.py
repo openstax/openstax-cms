2a66ab428ebeefb71e3a31d1a2adffc19b96389b
Michael Harrison <mwharrison@rice.edu>
2020-11-19 15:16:12 -0600

    update oxauth tests

--- oxauth/tests.py
+++ oxauth/tests.py
@@ -6,14 +6,15 @@ from django.test import TestCase, Client
 from django.conf import settings
 from django.urls import reverse
 
-from oxauth.functions import get_token, get_user_info
-from oxauth.views import login, get_user_data, logout
+from oxauth.functions import get_token, get_user_info, decrypt_cookie
+from oxauth.views import login, logout
 from .auth import OXSessionDecryptor
 
 
 class AccountsTestCase(TestCase):
     def setUp(self):
         self.client = Client()
+        self.sso_cookie = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..nPz-NXrgnVh9SyFJ.zQD5yD7RHQMNxJF1rpALVPlcaJv181Cc1ZTK-UUERWp8FM0ChbzfxUwCw3-5LOnHubnF2VJlLRdI3pPoNdcRXoHpnq5cwPX4z6NvTPeDt86r4zZwsgcW9iwrMos5077WEie91y1ruonnjU49BZWA5Ugk0fm82VSFkhGnXvjuDirJuBorZq6-Xp9-YiF5QEksUE_sT7DYTrGzSz_pa0fSlXMYMyslKDLlCuEihFurHTQnoMcA9LM6cZQ6TkAUC10so81wD44HabYz7cD5Q57aYM5GLh2sNrUKL1xBXGdaHipa_YuSc36JObGaZc9BS-t_nPv1aQpGeirLYRSWH06w9YwUEgAtR4fvZ5SbgdGkdxz15yVLyA8HD-BYz8SBflNRw91ZI3HnZbeY9M_Cpk_ejnujfsLyPsXSTa6mr8hKE3z8wtqcP-SodBvW2l75Ydg2MGkSo7_mcLLuXPGU7SqfVuwF1wIs8PV_Zg3ECjXlzeQ9WcTFzU-iF2wVpLZcMLOsy2o3WzJT4cWbWdva4RBp0Vp0JT-Bra6jPxS3TTKQIOHnqKfA4jpF_Szz2xIg_vwWAXQcejjMDpGbggCdvjvQqys1cP4Xb8vcZGfjKVzbYgJ_x4SKJw8LDDn294-HD9gTHroNqXv65Je1mp63M_QaF8hXLWTyIYTTfPema8sEO2HEB70X4UPV3qRICD6bwQi3KD4TveSU_q434n50mbU9W9EOZr2Fsc8jjJNfW0BRT_Z2lluC8aqvAkegS_abWboLdoaGnNNwwQAGaqmMvM3wnKVslPI-ew5s1tRVHs0dQz_T1Q0TOibDYln1h6CcSaeFprwXT8X-PMpYWJWoxWTW7NH8Ak_3lZ9nSGeJOdilLiwXJTsYgvuJDyEnXdHIjs5UgUovj_WxPigNpYn7BZT6feAAjUsKiGe2OEOvnJYhAKVHOQtvUhFUZOE6IiUdToytBaWroSILeLfY9B4JLXz6huXOKYOBWrdX4p1Naul-JHWvruSYsr5ja8ga7zZFgzUH3j82v-Kbfakv33IXNDJGHXGePo_f8AkB4Bw3Rohs1QJe7aTBBqMAUmUBsgboq4AL6_SCC1JQun-ScD9fcH2a1YXRucaplUGLvsThlhOiu1JkSEuhb_GpVFe4HprxOuF_c_0RHM_r0MpwjbkgfS49tqtE2sf05Qo2_iyE-hMl0ehmkyEe21ice1-0ogy7y-6m-STb5xXZzlSwghBhCwXTx8XgKvKDVHWQWw.btBy8lBSW-xKBuJO3geDRA"
 
     def test_accounts_contains_uuid(self):
         token = get_token()
@@ -27,18 +28,6 @@ class AccountsTestCase(TestCase):
             uuid = data['items'][0]['uuid']
 
         self.assertEqual(uuid, "aaa560a1-e828-48fb-b9a8-d01e9aec71d0")
-
-    def test_can_get_user_info(self):
-        user_data = get_user_info(2)
-        self.assertNotEqual(user_data, False)
-
-    def test_user_info_returns_false_with_no_uid(self):
-        user_data = get_user_info()
-        self.assertEqual(user_data, False)
-
-    def test_user_data_returns_false_when_invalid(self):
-        user_data = get_user_info('asdf')
-        self.assertEqual(user_data, False)
 
     def test_oauth_login_url(self):
         response = self.client.get(reverse('social:begin', args=['openstax']))
@@ -58,27 +47,6 @@ class AccountsTestCase(TestCase):
         response = self.client.get(reverse('logout') + "?next=foo")
         self.assertRedirects(response, "/accounts/logout/?r=foo", fetch_redirect_response=False)
 
-    def test_user_notloggedin(self):
-        response = self.client.get(reverse('user'))
-
-        response_readable = json.loads(response.content.decode())
-
-        self.assertNotEqual(response.status_code, 404)
-        self.assertIn("decryption", response_readable)
-        self.assertIn("logged_in", response_readable)
-        self.assertIn("cookie", response_readable)
-        self.assertIn("validation", response_readable)
-
-
-    # TODO: Fix the validation, and run the validation function as well.
-    # def test_validate_cookie(self):
-    #     decryptor = OXSessionDecryptor(secret_key_base="ea068a28cbe16b01fc22c2151c9d00a9883d772e178a05daa6469d4b531200cf168b091c1f551282ab8863715f5f4af72c87799ba85c99d6184436858a390e1d", encrypted_cookie_salt="encrypted cookie", encrypted_signed_cookie_salt="signed encrypted cookie")
-    #     cookie = "WmwwcDBlNmVhRmRTVGlwUFErTVZTL0FBOWE1VERZS1gvNE82dTFrSTR0TDNHL1VsRVVDTGExSUc4UFh0Z2h2UUxDKzE2RitBVVg5LzRqQkpuQ09ZUHlIWlBkME8zdHZUSStETHdoS1ZkR1VmVm0rQUhBTlFJS3VBbUZDbGh3c3BtSkQzTXlaNk00UCtISHJjcnhIeDV3Z0RORGZKZG91QzE3cnR4VzhvSnlKa0tNdGwrQTBTZkp4Z0c1cHJiS3FGS2Nhakw1ejFmM3F4ajhYbWtGNlZwQjVRNjdZYWJvSUozd0xGYk5JMEVDRWZWNlp0VHBsbE5KT0VodW1icFF6UHNCRzI1NHZrZzdGYWZnelRhcGk2WHQ0T1ZxdTYzckcyRWZlZ1pWWGpDVXNZVGFPeUViUWVrSGJ3YXlNcVo5UnI0Nnp2Q0U2V2lGWUd0bWNOQVg1NHdKa0EzTFlFZlkzUCt0YUlaK0xUczVOYU5pRE16Z3RqWlRZV1YwYzJvdzhNWXZmUGtRTnFFSVV0dDNGWStDMm91alEvRVd0YXNESkpJNzE5Y1liZ3k3L3lUdnFkWGdoUE9ENDBXRlA3Y0hWbFQ5cDJvRXUxdk1zQ2dYdTQvbmg1SDJkU1hPZnNZUkpOS2VnVnVFOGNmSWVvSHFQNmErNDF2NXZCVkVqeklqUUtJMVYxNGNHUk1XN1RmUXRsS1BOOWUyNjU5ZG02ZC9sWmxLU2ZMdmtpSU56Q0VzRlNWSVBWUXE2UnFDbUpCZndFLS1RV3RyQUYxMmw2ZWhOWGs4UXNqSGJBPT0%3D--1b058a0d696c016fae58f207bdbae1db6df8c312"
-    #     validate = decryptor.validate_cookie(cookie)
-    #     self.assertEqual(validate, True)
-
-    def test_decrypt_cookie(self):
-        decryptor = OXSessionDecryptor(secret_key_base="ea068a28cbe16b01fc22c2151c9d00a9883d772e178a05daa6469d4b531200cf168b091c1f551282ab8863715f5f4af72c87799ba85c99d6184436858a390e1d", encrypted_cookie_salt="encrypted cookie", encrypted_signed_cookie_salt="signed encrypted cookie")
-        cookie = "WmwwcDBlNmVhRmRTVGlwUFErTVZTL0FBOWE1VERZS1gvNE82dTFrSTR0TDNHL1VsRVVDTGExSUc4UFh0Z2h2UUxDKzE2RitBVVg5LzRqQkpuQ09ZUHlIWlBkME8zdHZUSStETHdoS1ZkR1VmVm0rQUhBTlFJS3VBbUZDbGh3c3BtSkQzTXlaNk00UCtISHJjcnhIeDV3Z0RORGZKZG91QzE3cnR4VzhvSnlKa0tNdGwrQTBTZkp4Z0c1cHJiS3FGS2Nhakw1ejFmM3F4ajhYbWtGNlZwQjVRNjdZYWJvSUozd0xGYk5JMEVDRWZWNlp0VHBsbE5KT0VodW1icFF6UHNCRzI1NHZrZzdGYWZnelRhcGk2WHQ0T1ZxdTYzckcyRWZlZ1pWWGpDVXNZVGFPeUViUWVrSGJ3YXlNcVo5UnI0Nnp2Q0U2V2lGWUd0bWNOQVg1NHdKa0EzTFlFZlkzUCt0YUlaK0xUczVOYU5pRE16Z3RqWlRZV1YwYzJvdzhNWXZmUGtRTnFFSVV0dDNGWStDMm91alEvRVd0YXNESkpJNzE5Y1liZ3k3L3lUdnFkWGdoUE9ENDBXRlA3Y0hWbFQ5cDJvRXUxdk1zQ2dYdTQvbmg1SDJkU1hPZnNZUkpOS2VnVnVFOGNmSWVvSHFQNmErNDF2NXZCVkVqeklqUUtJMVYxNGNHUk1XN1RmUXRsS1BOOWUyNjU5ZG02ZC9sWmxLU2ZMdmtpSU56Q0VzRlNWSVBWUXE2UnFDbUpCZndFLS1RV3RyQUYxMmw2ZWhOWGs4UXNqSGJBPT0%3D--1b058a0d696c016fae58f207bdbae1db6df8c312"
-        decrypted = decryptor.get_cookie_data(cookie)
-        self.assertEqual('session_id' in decrypted, True)
+    def test_can_decrypt_sso_cookie(self):
+        decrypted_cookie = decrypt_cookie(self.sso_cookie)
+        self.assertEqual(decrypted_cookie.user_uuid, '467cea6c-8159-40b1-90f1-e9b0dc26344c')
