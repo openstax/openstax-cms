deb8229c0f1f2a61e24ffe1e4b080b6ccff0e1b8
Michael Harrison <mwharrison@rice.edu>
2020-11-20 10:17:09 -0600

    process deletions in salesforce / some error handling

--- salesforce/management/commands/sync_reviews.py
+++ salesforce/management/commands/sync_reviews.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from salesforce.models import PartnerReview
 from salesforce.salesforce import Salesforce
+from simple_salesforce.exceptions import SalesforceGeneralError
 
 
 class Command(BaseCommand):
@@ -10,7 +11,7 @@ class Command(BaseCommand):
         with Salesforce() as sf:
 
             # To update the existing reviews with Partner responses and approved reviews.
-            command = "Select Id, Status__c, Approved_Customer_Review__c, Pending_Customer_Review__c, Partner_Response__c, Partner__c, Contact__c, Score__c, OS_Accounts_ID__c FROM Partner_Review__c WHERE Status__c = 'Approved' OR Status__c = 'Responded'"
+            command = "Select Id, Status__c, Approved_Customer_Review__c, Pending_Customer_Review__c, Partner_Response__c, Partner_Response_Date__c, Partner__c, Contact__c, Score__c, OS_Accounts_ID__c FROM Partner_Review__c WHERE Status__c = 'Approved' OR Status__c = 'Responded'"
             response = sf.query_all(command)
             sf_reviews = response['records']
 
@@ -19,6 +20,7 @@ class Command(BaseCommand):
                     review = PartnerReview.objects.get(review_salesforce_id=record['Id'])
                     review.review = record['Approved_Customer_Review__c']
                     review.partner_response = record['Partner_Response__c']
+                    review.partner_response_date = record['Partner_Response_Date__c']
                     review.status = 'Approved'
                     review.save()
                 except PartnerReview.DoesNotExist:
@@ -36,9 +38,13 @@ class Command(BaseCommand):
                     'OS_Accounts_ID__c': review.submitted_by_account_id,
                     'Score__c': review.rating,
                 }
-                response = sf.Partner_Review__c.create(data)
-                review.review_salesforce_id = response['id']
-                review.save()
+                try:
+                    response = sf.Partner_Review__c.create(data)
+                    review.review_salesforce_id = response['id']
+                    review.save()
+                except SalesforceGeneralError:
+                    print('Failed to create review in Salesforce - are you sure the partner exists?')
+
 
 
             # If a review is in the Edited state, it needs to be reuploaded to SF and set to 'New'
@@ -50,9 +56,19 @@ class Command(BaseCommand):
                     'OS_Accounts_ID__c': review.submitted_by_account_id,
                     'Score__c': review.rating,
                 }
-                response = sf.Partner_Review__c.update(data=data, record_id=review.review_salesforce_id)
-                review.status = 'Awaiting Approval'
-                review.save()
+                try:
+                    response = sf.Partner_Review__c.update(data=data, record_id=review.review_salesforce_id)
+                    review.status = 'Awaiting Approval'
+                    review.save()
+                except SalesforceGeneralError:
+                    print('Failed to update review in Salesforce')
+
+            # Finally, if a review was deleted by a user, let's delete it from Salesforce.
+            reviews_to_delete = PartnerReview.objects.filter(status='Deleted')
+            for review in reviews_to_delete:
+                if review.review_salesforce_id:
+                    sf.Partner_Review__c.delete(review.review_salesforce_id)
+                review.delete()
 
 
             response = self.style.SUCCESS("Successfully updated partner reviews")
