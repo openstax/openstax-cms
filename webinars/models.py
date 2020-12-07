1b8c6f643b801af25472be26cbb79236d08b9a50
Michael Harrison <mwharrison@rice.edu>
2020-12-07 10:15:21 -0600

    tutor page redesign (#1105)

--- webinars/models.py
+++ webinars/models.py
@@ -9,6 +9,7 @@ class Webinar(models.Model):
     spaces_remaining = models.PositiveIntegerField()
     registration_url = models.URLField()
     registration_link_text = models.CharField(max_length=255)
+    display_on_tutor_page = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
