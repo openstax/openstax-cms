af113d65229829704169c12924c1e8e4ddf06ad6
Ed Woodward <ecw1@rice.edu>
2021-05-06 13:01:39 -0500

    Commiting to code on another branch

--- salesforce/models.py
+++ salesforce/models.py
@@ -330,6 +330,7 @@ class ResourceDownload(models.Model):
     last_access = models.DateTimeField()
     number_of_times_accessed = models.IntegerField()
     resource_name = models.CharField(max_length=255, null=True, blank=False)
+    contact_id = models.CharField(max_length=100, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.pk:
