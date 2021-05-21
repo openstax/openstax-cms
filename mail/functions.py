0a2e25da10f2f5a3ffe34c1710246909ae416ef2
Ed Woodward <ecw1@rice.edu>
2021-05-21 14:44:37 -0500

    Removed dev email about redirect report

--- mail/functions.py
+++ mail/functions.py
@@ -20,21 +20,3 @@ def send_redirect_report(bad_redirects):
 
     except Exception as e:
         logger.error("EMAIL FAILED TO SEND: subject:{}".format(subject))
-
-def send_dev_email(email_subject, data_to_send):
-    msg = 'Dev Email - ' + email_subject
-    msg += '\n\n' + str(data_to_send)
-
-    subject = email_subject
-    from_address = 'noreply@openstax.org'
-    to_address = ['bitdevs@openstax.org', ]
-
-    try:
-        email = EmailMessage(subject,
-                             msg,
-                             from_address,
-                             to_address)
-        email.send()
-
-    except Exception as e:
-        logger.error("EMAIL FAILED TO SEND: subject:{}".format(subject))
