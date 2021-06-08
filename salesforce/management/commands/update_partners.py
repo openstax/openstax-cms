c3bf89be9f9aa0268a9728b2cb9127db2830f8d1
Ed Woodward <ecw1@rice.edu>
2021-06-08 07:00:05 -0500

    initial changes before migration

--- salesforce/management/commands/update_partners.py
+++ salesforce/management/commands/update_partners.py
@@ -113,6 +113,7 @@ class Command(BaseCommand):
                     "Online_teaching_academic_integrity__c, " \
                     "Online_teaching_labs__c, " \
                     "International__c " \
+                    "Full_partner__c " \
                     "FROM Account WHERE RecordTypeId = '012U0000000MeAuIAK'"
             response = sf.query_all(query)
             sf_marketplace_partners = response['records']
@@ -225,6 +226,7 @@ class Command(BaseCommand):
                 p.online_teaching_academic_integrity=self.str2bool(partner['Online_teaching_academic_integrity__c'])
                 p.online_teaching_teaching_labs=self.str2bool(partner['Online_teaching_labs__c'])
                 p.international=self.str2bool(partner['International__c'])
+                p.full_partner = self.str2bool(partner['Full_partner__c'])
                 p.save()
                 updated_partners = updated_partners + 1
 
