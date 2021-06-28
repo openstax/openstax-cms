56fba2b35e78e8751c461bd8e14191364ec50b8b
Michael Harrison <mwharrison@rice.edu>
2021-06-28 16:22:25 -0500

    invalidate cache on donation popup save

---
+++ donations/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from wagtail.core.signals import page_published
+
+from global_settings.functions import invalidate_cloudfront_caches
+from .models import DonationPopup
+
+@receiver(post_save, sender=DonationPopup)
+def clear_cloudfront_on_donation_popup_save(sender, **kwargs):
+    invalidate_cloudfront_caches()
