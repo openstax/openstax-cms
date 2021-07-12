f0879f5080e0e3521ce21c8f071a75ebe773831d
Colby <colby.powers27@gmail.com>
2021-07-12 10:23:28 -0500

    Update models.py

--- errata/models.py
+++ errata/models.py
@@ -339,7 +339,7 @@ def send_status_update_email(sender, instance, created, **kwargs):
         override_to = False
 
         if created:
-            if instance.book.book_state == 'New Edition Forthcoming (Show new edition correction schedule)':
+            if instance.book.book_state == 'new_edition_available':
                 email_text = EmailText.objects.get(email_case='Getting New Edition')
                 subject = email_text.email_subject_text
                 body = email_text.email_body_text
