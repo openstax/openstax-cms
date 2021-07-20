0315e80493657e8807cf5380e725be95f85d9e6e
Colby <colby.powers27@gmail.com>
2021-07-20 13:01:27 -0500

    adding validation for associate status

--- errata/models.py
+++ errata/models.py
@@ -226,7 +226,7 @@ class Errata(models.Model):
     def clean(self):
         if self.status == 'Completed' and not self.resolution or self.status == 'Reviewed' and not self.resolution:
             raise ValidationError({'resolution': 'Resolution is required if status is completed or reviewed.'})
-        if (self.status == 'Editorial Review' or self.status == 'K-12 Editorial Review' or self.status == 'Kelsey Editorial Review' or self.status == 'Anthony Editorial Review' or self.status == 'Reviewed' or self.status == 'Completed') and not self.is_assessment_errata:
+        if (self.status == 'Editorial Review' or self.status == 'K-12 Editorial Review' or self.status == 'Associate Editorial Review' or self.status == 'Anthony Editorial Review' or self.status == 'Reviewed' or self.status == 'Completed') and not self.is_assessment_errata:
             raise ValidationError({'is_assessment_errata': 'You must specify if this is an assessment errata.'})
         if (self.status == 'Completed' and self.resolution == 'Duplicate') and not self.duplicate_id:
             raise ValidationError({'duplicate_id': 'You must specify the duplicate report ID when resolution is marked duplicate.'})
