1889cd9747fffaa8b3cbdccca91213221769a806
Ed Woodward <ecw1@rice.edu>
2022-02-23 11:50:18 -0600

    command to create subject pages

--- pages/management/commands/create_subjects_page.py
+++ pages/management/commands/create_subjects_page.py
@@ -1,6 +1,6 @@
 import json
 from django.core.management.base import BaseCommand
-from pages.models import Subjects
+from pages.models import HomePage, Subjects
 
 
 class Command(BaseCommand):
@@ -14,19 +14,19 @@ class Command(BaseCommand):
             description="North, South, East, West, our subjects are the best!",
             tutor_ad=json.dumps([
                 {"type": "content",
-                  "value": {heading="INSTRUCTORS, TAKE YOUR COURSE ONLINE",
-                            ad_html='<strong>Assign homework and readings synced with OpenStax textbooks</strong>',
-                            link_text='Learn more',
-                            link_href='https://openstax.org/tutor'
+                  "value": {heading:"INSTRUCTORS, TAKE YOUR COURSE ONLINE",
+                            ad_html:"<strong>Assign homework and readings synced with OpenStax textbooks</strong>",
+                            link_text:"Learn more",
+                            link_href:"https://openstax.org/tutor"
                             },
                 }
             ]),
             about_os=json.dumps([
                 {"type": "content",
-                  "value": {heading="About Openstax textbooks",
-                            os_text='OpenStax is part of Rice University, which is a 501(c)(3) nonprofit charitable corporation. Our mission is to improve educational access and learning for everyone. We do this by publishing openly licensed books, developing and improving research-based courseware, establishing partnerships with educational resource companies, and more.',
-                            link_text='Learn about OpenStax',
-                            link_href='https://openstax.org/about-us'
+                  "value": {heading:"About Openstax textbooks",
+                            os_text:'OpenStax is part of Rice University, which is a 501(c)(3) nonprofit charitable corporation. Our mission is to improve educational access and learning for everyone. We do this by publishing openly licensed books, developing and improving research-based courseware, establishing partnerships with educational resource companies, and more.',
+                            link_text:'Learn about OpenStax',
+                            link_href:'https://openstax.org/about-us'
                             },
                 }
             ]),
@@ -38,10 +38,9 @@ class Command(BaseCommand):
                                "text": "All textbooks meet standard scope and sequence requirements, making them seamlessly adaptable into existing courses.."},
                              {"heading": "Peer Reviewed",
                                "text": "OpenStax textbooks undergo a rigorous peer review process. You can view the list of contributors when you click on each book."},
-                            ]}
-                           }
+                           ]}
                 }
-            ])
+            ]),
             philanthropic_support="With philanthropic support, our books have been used in <strong>38,160<strong> classrooms, saving students <strong>$1,747,190,405<strong> since 2012. <a href='https://openstax.org/impact'>Learn more about our impact</a> and how you can help."
         )
         homepage.add_child(instance=subjects_page)
