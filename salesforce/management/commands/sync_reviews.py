43baa96417008993ebe5e252b749ff6aa9b29b69
Ed Woodward <ecw1@rice.edu>
2022-03-28 07:51:27 -0500

    Fix for deleted reviews getting set back to not deleted incorrectly (#1263)

--- salesforce/management/commands/sync_reviews.py
+++ salesforce/management/commands/sync_reviews.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
             for record in sf_reviews:
                 try:
                     review = PartnerReview.objects.get(review_salesforce_id=record['Id'])
-                    if review.status != 'Edited':
+                    if review.status != 'Edited' and review.status != 'Deleted':
                         review.review = record['Approved_Customer_Review__c']
                         review.partner_response = record['Partner_Response__c']
                         review.partner_response_date = record['Partner_Response_Date__c']
