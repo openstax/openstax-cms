46fafd3757e7998070a6b9e5b8f76c04f47a4b9e
Dante Soares <dante.m.soares@rice.edu>
2022-04-14 09:27:03 -0500

    Added a healthcheck middleware so we can ping django instead of just nginx

---
+++ healthcheck/middleware.py
@@ -0,0 +1,11 @@
+# https://stackoverflow.com/a/64623669
+from django.http import HttpResponse
+
+class HealthCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path == '/ping':
+            return HttpResponse('ok')
+        return self.get_response(request)
