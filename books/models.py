818cb9874a16aa4c024b419643edc9bb08b35bee
Ed Woodward <ecw1@rice.edu>
2022-04-15 10:23:58 -0500

    More data checks before accessing categories

--- books/models.py
+++ books/models.py
@@ -832,8 +832,10 @@ class Book(Page):
     @property
     def subject_categories(self):
         category_list = []
-        for category in self.book_categories.all():
-            category_list.append(category.subject_category)
+        if self.book_categories is not None:
+            for category in self.book_categories.all():
+                if category.subject_category is not None:
+                    category_list.append(category.subject_category)
         return category_list
 
     @property
