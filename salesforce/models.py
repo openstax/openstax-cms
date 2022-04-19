71a5bab3cb131638361615d221a9decc9b1977aa
Ed Woodward <ecw1@rice.edu>
2022-04-19 11:15:42 -0500

    Removed salesforce id and simplified SF update for resource downloads

--- salesforce/models.py
+++ salesforce/models.py
@@ -386,7 +386,6 @@ class ResourceDownload(models.Model):
         ('Bookshare', 'Bookshare'),
         ('Chegg Reader', 'Chegg Reader'),
     )
-    salesforce_id = models.CharField(max_length=100, blank=True, null=True)
     book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True, blank=True)
     book_format = models.CharField(max_length=100, choices=BOOK_FORMATS, null=True , blank=True)
     account_id = models.IntegerField(blank=True, null=True) # TODO: remove this field after migrating data to UUID (see management command)
