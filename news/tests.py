7afec20e8debbaff3a57ed86a70561858cd20695
Ed Woodward <ecw1@rice.edu>
2022-05-06 14:54:57 -0500

    Tests are now passing

--- news/tests.py
+++ news/tests.py
@@ -13,66 +13,6 @@ from snippets.models import Subject, BlogContentType, BlogCollection
 
 class NewsTests(WagtailPageTests, TestCase):
     def setUp(self):
-        self.math = Subject(name="Math", page_content="Math page content.", seo_title="Math SEO Title",
-                            search_description="Math page description.")
-        self.math.save()
-
-        self.economics = Subject(name="Economics", page_content="Economics page content.",
-                                 seo_title="Economics SEO Title",
-                                 search_description="Economics page description.")
-        self.economics.save()
-
-        self.case_study = BlogContentType(content_type='Case Study')
-        self.case_study.save()
-
-    @classmethod
-    def setUpTestData(cls):
-        # create root page
-        root_page = Page.objects.get(title="Root")
-        # create homepage
-        homepage = HomePage(title="Hello World",
-                            slug="hello-world",
-                            )
-        # add homepage to root page
-        root_page.add_child(instance=homepage)
-        # create book index page
-        news_index = NewsIndex(title="News Index")
-        # add book index to homepage
-        homepage.add_child(instance=news_index)
-
-        cls.news_index = Page.objects.get(id=news_index.id)
-
-    def test_can_create_news_article(self):
-        news_index = NewsIndex.objects.all()[0]
-        article = NewsArticle(title="Article 1",
-                              slug="article",
-                              date=timezone.now(),
-                              heading="Sample Article",
-                              subheading="Sample Subheading",
-                              author="OpenStax")
-        news_index.add_child(instance=article)
-        self.assertEqual(article.heading, "Sample Article")
-
-    def test_bad_slug_returns_404(self):
-        response = self.client.get('/apps/cms/api/news/bad-slug/', format='json')
-        self.assertEqual(response.status_code, 404)
-
-    def test_slashless_apis_are_good(self):
-        NewsIndex.objects.all = MagicMock(return_value=MagicMock(pk=3))
-        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/news')
-
-        PressIndex.objects.all = MagicMock(return_value=MagicMock(pk=3))
-        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/press')
-
-        NewsArticle.objects.get = MagicMock(return_value=MagicMock(pk=3))
-        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/news/slug')
-
-        PressRelease.objects.get = MagicMock(return_value=MagicMock(pk=3))
-        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/press/slug')
-
-
-class SearchTest(TestCase):
-    def setUp(self):
         # create collections
         self.learning = BlogCollection(name='Teaching and Learning', description='this is a collection')
         self.learning.save()
@@ -103,58 +43,62 @@ class SearchTest(TestCase):
         # create news articles
         news_index = NewsIndex.objects.all()[0]
         self.article = NewsArticle(title="Article 1",
-                              slug="article",
-                              date=timezone.now(),
-                              heading="Sample Article",
-                              subheading="Sample Subheading",
-                              author="OpenStax",
-                              body=json.dumps(
-                                  [
-                                      {"type": "paragraph", "value": "<p>This is the body of the post</p><p>This is the second paragraph</p>"}
-                                  ]
-                              ),
-                              article_subjects=json.dumps(
-                                  [
-                                      {"type": "subject", "value": [{"subject": math_id}]}
-                                  ]
-                              ),
-                              content_types=json.dumps(
-                                  [
-                                      {"type": "content_type", "value": [{"content_type": case_study_id}]},
-                                  ]
-                              ),
-                              collections=json.dumps(
-                                    [
-                                        {"type": "collection", "value": [{"collection": learning_id, "featured": 'false', "popular": 'false'}]}
-                                    ]
-                              ))
+                                   slug="article",
+                                   date=timezone.now(),
+                                   heading="Sample Article",
+                                   subheading="Sample Subheading",
+                                   author="OpenStax",
+                                   body=json.dumps(
+                                       [
+                                           {"type": "paragraph",
+                                            "value": "<p>This is the body of the post</p><p>This is the second paragraph</p>"}
+                                       ]
+                                   ),
+                                   article_subjects=json.dumps(
+                                       [
+                                           {"type": "subject", "value": [{"subject": math_id}]}
+                                       ]
+                                   ),
+                                   content_types=json.dumps(
+                                       [
+                                           {"type": "content_type", "value": [{"content_type": case_study_id}]},
+                                       ]
+                                   ),
+                                   collections=json.dumps(
+                                       [
+                                           {"type": "collection", "value": [
+                                               {"collection": learning_id, "featured": 'false', "popular": 'false'}]}
+                                       ]
+                                   ))
         news_index.add_child(instance=self.article)
         self.article2 = NewsArticle(title="Article 2",
-                              slug="article2",
-                              date=timezone.now(),
-                              heading="Sample Article 2",
-                              subheading="Sample Subheading 2",
-                              author="OpenStax",
-                              body=json.dumps(
-                                  [
-                                      {"type": "paragraph", "value": "<p>This is the body of the post</p><p>This is the second paragraph</p>"}
-                                  ]
-                              ),
-                              article_subjects=json.dumps(
-                                  [
-                                      {"type": "subject", "value": [{"subject": economics_id}]}
-                                  ]
-                              ),
-                              content_types=json.dumps(
-                                  [
-                                      {"type": "content_type", "value": [{"content_type": video_id}]},
-                                  ]
-                              ),
-                              collections=json.dumps(
-                                    [
-                                        {"type": "collection", "value": [{"collection": update_id, "featured": 'false', "popular": 'false'}]}
-                                    ]
-                              ))
+                                    slug="article2",
+                                    date=timezone.now(),
+                                    heading="Sample Article 2",
+                                    subheading="Sample Subheading 2",
+                                    author="OpenStax",
+                                    body=json.dumps(
+                                        [
+                                            {"type": "paragraph",
+                                             "value": "<p>This is the body of the post</p><p>This is the second paragraph</p>"}
+                                        ]
+                                    ),
+                                    article_subjects=json.dumps(
+                                        [
+                                            {"type": "subject", "value": [{"subject": economics_id}]}
+                                        ]
+                                    ),
+                                    content_types=json.dumps(
+                                        [
+                                            {"type": "content_type", "value": [{"content_type": video_id}]},
+                                        ]
+                                    ),
+                                    collections=json.dumps(
+                                        [
+                                            {"type": "collection", "value": [
+                                                {"collection": update_id, "featured": 'false', "popular": 'false'}]}
+                                        ]
+                                    ))
         news_index.add_child(instance=self.article2)
         self.article3 = NewsArticle(title="Article 3",
                                     slug="article3",
@@ -203,6 +147,23 @@ class SearchTest(TestCase):
 
         cls.news_index = Page.objects.get(id=news_index.id)
 
+    def test_bad_slug_returns_404(self):
+        response = self.client.get('/apps/cms/api/news/bad-slug/', format='json')
+        self.assertEqual(response.status_code, 404)
+
+    def test_slashless_apis_are_good(self):
+        NewsIndex.objects.all = MagicMock(return_value=MagicMock(pk=3))
+        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/news')
+
+        PressIndex.objects.all = MagicMock(return_value=MagicMock(pk=3))
+        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/press')
+
+        NewsArticle.objects.get = MagicMock(return_value=MagicMock(pk=3))
+        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/news/slug')
+
+        PressRelease.objects.get = MagicMock(return_value=MagicMock(pk=3))
+        assertPathDoesNotRedirectToTrailingSlash(self, '/apps/cms/api/press/slug')
+
     def test_search_blog_collection(self):
         response = self.client.get('/apps/cms/api/search/', {'collection': 'OpenStax Updates'})
         self.assertContains(response, 'OpenStax Updates')
@@ -238,4 +199,5 @@ class SearchTest(TestCase):
         response = self.client.get('/apps/cms/api/search/', {'collection': 'OpenStax Updates', 'subjects': 'Economics,Math'})
         self.assertContains(response, 'Economics')
         self.assertContains(response, 'Math')
+
 
