7b2e66fef209ff29916d6028aa7a74abf41ccbd8
Michael Harrison <volo@rice.edu>
2022-05-06 13:12:15 -0500

    add coverage to requirements, test all manage.py test runs

--- manage.py
+++ manage.py
@@ -2,9 +2,40 @@
 import os
 import sys
 
-if __name__ == "__main__":
+def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openstax.settings")
 
-    from django.core.management import execute_from_command_line
+    # MyProject Customization: run coverage.py around tests automatically
+    try:
+        command = sys.argv[1]
+    except IndexError:
+        command = "help"
 
+    running_tests = command == "test"
+    if running_tests:
+        from coverage import Coverage
+
+        cov = Coverage()
+        cov.erase()
+        cov.start()
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
+
+    if running_tests:
+        cov.stop()
+        cov.save()
+        covered = cov.report()
+        if covered < 100:
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
