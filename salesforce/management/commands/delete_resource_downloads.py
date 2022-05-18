15541c06b72500bcd838d0e73ed064d035d3fc5c
Ed Woodward <ecw1@rice.edu>
2022-05-18 10:19:14 -0500

    Using created date for delete and removed unneeded imports

--- salesforce/management/commands/delete_resource_downloads.py
+++ salesforce/management/commands/delete_resource_downloads.py
@@ -1,8 +1,5 @@
 from django.core.management.base import BaseCommand
-#from django.db import transaction
 from salesforce.models import ResourceDownload
-from salesforce.salesforce import Salesforce
-#from simple_salesforce.exceptions import SalesforceResourceNotFound
 from django.utils import timezone
 from datetime import timedelta
 
@@ -12,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         delete_from_date = timezone.now() - timedelta(days=7)
-        downloads_to_delete = ResourceDownload.objects.filter(last_access__lte=delete_from_date)
+        downloads_to_delete = ResourceDownload.objects.filter(created__lte=delete_from_date)
         num_deleted = len(downloads_to_delete)
         downloads_to_delete.delete()
         response = self.style.SUCCESS("{} resource downloads greater than 7 days old deleted from CMS database".format(num_deleted))
