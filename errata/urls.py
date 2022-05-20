49725c37937f6ee4438352223bba4741793cd8a6
Michael Harrison <volo@rice.edu>
2022-05-19 19:17:34 -0500

    extreme - but going to remove the unused errata admin dashboard to improve test coverage (not modified in 4+ years)

--- errata/urls.py
+++ errata/urls.py
@@ -6,8 +6,5 @@ router = routers.SimpleRouter()
 router.register(r'', views.ErrataView)
 
 urlpatterns = [
-    path('admin/dashboard/', views.dashboard, name='errata_dashboard'),
-    path('admin/list/', views.list, name='errata_list'),
-    path('admin/edit/', views.edit, name='errata_edit'),
     path(r'', include(router.urls)),
 ]
