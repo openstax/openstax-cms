a25f8bdadf7b9c5bb6c4271988526cc69dd2271e
Ed Woodward <ecw1@rice.edu>
2022-06-21 16:06:34 -0500

    removed unneeded code

--- webinars/views.py
+++ webinars/views.py
@@ -1,20 +1,8 @@
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
-from .models import Webinar
 from .serializers import WebinarSerializer
 
 
 class WebinarViewSet(viewsets.ViewSet):
     serializer_class = WebinarSerializer
 
-    @action(methods=['get'], detail=True)
-    def list(self, request):
-        queryset = Webinar.objects.all()
-        subject = request.query_params.get('subject', None)
-        if subject is not None:
-            queryset = queryset.filter(webinar_subjects=subject)
-
-        serializer = WebinarSerializer(queryset, many=True)
-        return Response(serializer.data)
