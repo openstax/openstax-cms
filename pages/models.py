e21e1552945f1d2e5bc72c1daade8c67127dd1bc
Ed Woodward <ecw1@rice.edu>
2022-07-06 12:55:23 -0500

    Added filter for tutor books

--- pages/models.py
+++ pages/models.py
@@ -2701,12 +2701,13 @@ class TutorLanding(Page):
         books = Book.objects.filter(tutor_marketing_book=True).order_by('path')
         book_data = []
         for book in books:
-            book_data.append({
-                'id': book.id,
-                'slug': 'books/{}'.format(book.slug),
-                'title': book.title,
-                'cover_url': book.cover_url,
-            })
+            if book.book_state is not 'Retired' and book.book_state is not 'Draft':
+                book_data.append({
+                    'id': book.id,
+                    'slug': 'books/{}'.format(book.slug),
+                    'title': book.title,
+                    'cover_url': book.cover_url,
+                })
         return book_data
 
     api_fields = [
