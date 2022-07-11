a6388ef935d92c7c3134fd35b403a06dde5d68a1
Ed Woodward <ecw1@rice.edu>
2022-07-11 13:45:23 -0500

    Passing parameter to book API

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
 from random import randrange
+from urllib.parse import urlparse
+
 from django.shortcuts import redirect
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
@@ -13,8 +15,18 @@ def book_index(request):
 
 @csrf_exempt
 def book_detail(request, slug):
+    x_param = request.GET.get('x', False)
+    print(str(request))
+    print('x_param: ' + str(x_param))
+    # print('request.path: ' + str(request.path))
+    # if '?' in request.path:
+    #     query = urlparse(request.path).query
+    #     print('query:' + str(query))
     try:
         page = Book.objects.get(slug=slug)
-        return redirect('/apps/cms/api/v2/pages/{}/'.format(page.pk))
+        if x_param == 'y':
+            return redirect('/apps/cms/api/v2/pages/{}/?x=y'.format(page.pk))
+        else:
+            return redirect('/apps/cms/api/v2/pages/{}/'.format(page.pk))
     except Book.DoesNotExist:
         raise Http404("Book does not exist.")
