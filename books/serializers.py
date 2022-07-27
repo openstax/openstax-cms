27ed1d3c89714c31c7a1b55e27722a9496e125d7
Ed Woodward <ecw1@rice.edu>
2022-07-27 12:02:28 -0500

    Limit access to faculty resources based on user status

---
+++ books/serializers.py
@@ -0,0 +1,23 @@
+from .models import Book
+from rest_framework import serializers
+
+
+class FacultyResourcesSerializer(serializers.ModelSerializer):
+    # def to_representation(self, instance):
+    #     ret = super().to_representation(instance)
+    #     print('ret: ' + str(ret))
+    #     request = self.context['request']
+    #     print('request: ' + str(request))
+    #     x_param = request.GET.get('x', False)
+    #     if x_param and x_param == 'y':
+    #         bfr = ret['book_faculty_resources']
+    #         if not bfr['resource_unlocked']:
+    #             bfr['link_document_url'] = ''
+    #             bfr['link_external'] = ''
+    #             bfr['link_page'] = ''
+    #             ret['book_faculty_resources'] = bfr
+    #     return ret
+    class Meta:
+        model = Book
+        fields = ('book_video_faculty_resources','book_orientation_faculty_resources','book_faculty_resources')
+        read_only_fields = ('book_video_faculty_resources','book_orientation_faculty_resources','book_faculty_resources')
--- books/views.py
+++ books/views.py
@@ -4,7 +4,13 @@ from urllib.parse import urlparse
 from django.shortcuts import redirect
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from openstax.functions import remove_locked_links_detail
 from .models import BookIndex, Book
+from .serializers import FacultyResourcesSerializer
 
 
 @csrf_exempt
@@ -30,3 +36,25 @@ def book_detail(request, slug):
             return redirect('/apps/cms/api/v2/pages/{}/'.format(page.pk))
     except Book.DoesNotExist:
         raise Http404("Book does not exist.")
+
+# @csrf_exempt
+# def resources(request, slug):
+#     x_param = request.GET.get('x', False)
+#     #slug = request.GET.get('slug', False)
+#     try:
+#         page = Book.objects.get(slug=slug)
+#         return remove_locked_links_detail(Response(page.data))
+#
+#     except Book.DoesNotExist:
+#         raise Http404("Book does not exist.")
+
+
+class ResourcesViewSet(viewsets.ViewSet):
+    @action(methods=['get'], detail=True)
+    def list(self, request):
+        slug = request.GET.get('slug', False)
+        queryset = Book.objects.filter(slug=slug)
+        print('queryset: ' + str(queryset))
+        serializer = FacultyResourcesSerializer(queryset[0], context={'request': request})
+        return Response(serializer.data)
+
