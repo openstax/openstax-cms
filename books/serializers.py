c1f162f67644db777b6b87f70d2cde08379daf82
Ed Woodward <ecw1@rice.edu>
2022-08-05 16:22:06 -0500

    Code now working, but more testing needed

--- books/serializers.py
+++ books/serializers.py
@@ -5,17 +5,37 @@ from rest_framework import serializers
 class FacultyResourcesSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ret = super().to_representation(instance)
-        print('ret: ' + str(ret))
         request = self.context['request']
-        #print('request: ' + str(request))
         x_param = request.GET.get('x', False)
-        if x_param and x_param == 'y':
-            bfr = ret['book_faculty_resources'].values()
-            if not bfr['unlocked_resource']:
-                bfr['link_document_url'] = ''
-                bfr['link_external'] = ''
-                bfr['link_page'] = ''
-                ret['book_faculty_resources'] = bfr
+        
+        bfr = ret['book_faculty_resources']
+        for resource in bfr:
+            # remove listing of linked book data
+            resource['book_faculty_resource'] = {}
+            if x_param and x_param == 'y':
+                if not resource['resource']['unlocked_resource']:
+                    if resource['link_document'] is not None:
+                        resource['link_document']['file'] = ''
+                    if resource['link_page'] is not None:
+                        resource['link_page']['url_path'] = ''
+
+        bofr = ret['book_orientation_faculty_resources']
+        for resource in bofr:
+            # remove listing of linked book data
+            resource['book_orientation_faculty_resource'] = {}
+            if x_param and x_param == 'y':
+                if not resource['resource_unlocked']:
+                    if resource['link_external'] is not None:
+                        resource['link_external'] = ''
+                    if resource['link_page'] is not None:
+                        resource['link_page'] = ''
+                    if resource['link_document'] is not None:
+                        resource['link_document']['file'] = ''
+
+        bvfr = ret['book_video_faculty_resources']
+        for resource in bvfr:
+            # remove listing of linked book data
+            resource['book_video_faculty_resource'] = {}
         return ret
 
     class Meta:
