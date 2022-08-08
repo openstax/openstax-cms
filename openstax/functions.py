afa720c4566a6bbdb37457b062ef6c77c096915c
Ed Woodward <ecw1@rice.edu>
2022-08-08 13:08:29 -0500

    Removed code in functions that is no longer needed

--- openstax/functions.py
+++ openstax/functions.py
@@ -26,48 +26,32 @@ def build_image_url(image):
     else:
         return None
 
+
 def remove_locked_links_detail(response):
     """
     Checks whether the response has a key 'book_faculty_resources',
     and if so, removes the 'link_document_url' and 'link_external' if 'resource_unlocked'
     is set to True.
-
     Returns True if at least a link is hidden.
     """
 
     any_hidden = False
-    print('response: ' + str(response.data.book_faculty_resources.values()[0]))
-    faculty_resources = []
-    video_resources = []
-    orientation_resources = []
-    for faculty_resource in response.data.book_faculty_resources.values():
 
-        #print(FacultyResources.objects.filter(id=faculty_resource['link_document_id']).values())
-        faculty_resources.append(faculty_resource)
+    if "book_faculty_resources" in response.data:
+        for res_id in range(len(response.data["book_faculty_resources"])):
+            if not response.data["book_faculty_resources"][res_id]["resource_unlocked"]:
+                response.data["book_faculty_resources"][res_id]["link_document_url"] = ""
+                response.data["book_faculty_resources"][res_id]["link_external"] = ""
+                any_hidden = True
 
-    faculty_resource_json = {}
-    faculty_resource_json['book_faculty_resources'] = faculty_resources
-    faculty_resource_json['book_video_faculty_resources'] = video_resources
-    faculty_resource_json['book_orientation_faculty_resources'] = faculty_resources
-    #faculty_resource_json.append({response.data.book_faculty_resources.values()})
-    #faculty_resource_json.append({response.data.book_video_faculty_resources.values()})
-    #faculty_resource_json.append({response.data.book_orientation_faculty_resources.values()})
+    return any_hidden
 
-    # if response.data.book_faculty_resources:
-    #     for resource in response.data.book_faculty_resources:
-    #         if not resource["resource_unlocked"]:
-    #             resource["link_document_url"] = ""
-    #             resource["link_external"] = ""
-    #             any_hidden = True
-                
-    return JsonResponse(faculty_resource_json, safe=False)
 
 def remove_locked_links_listing(response):
     """
     Checks whether the any response in all responses has a key 'book_faculty_resources',
     and if so, removes the 'link_document_url' and 'link_external' if 'resource_unlocked'
     is set to True.
-
     Returns True if at least a link is hidden.
     """
 
