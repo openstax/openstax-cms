f656cef8e1ad721879eef350b7099dbec8008b6c
Michael Harrison <volo@rice.edu>
2023-02-08 15:46:02 -0600

    adding post render callback to ErrataAdmin form

--- errata/forms.py
+++ errata/forms.py
@@ -5,6 +5,10 @@ class ErrataForm(forms.ModelForm):
     class Media:
         js = ('errata/errata-admin-ui.js', )
 
+    def add_post_render_callback(self):
+        pass
+
+
 
 class ErrataModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
