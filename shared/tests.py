1967b1bce216f15d4fdee3bf92c6528ce68887d0
ed woodward <ecw1@rice.edu>
2023-09-05 16:02:59 -0500

    Tests for mock functions

--- shared/tests.py
+++ shared/tests.py
@@ -1,8 +1,17 @@
-from wagtail.test.utils import WagtailPageTests
-from shared.test_utilities import assertPathDoesNotRedirectToTrailingSlash
+from django.test import TestCase
+from shared.test_utilities import mock_user_login, RequestMock
 from unittest.mock import MagicMock
 
-class WagtailTests(WagtailPageTests):
-    def setUp(self):
-        pass
+class TestMockUserLogin(TestCase):
+    def test_mock_user_login(self):
+        user = mock_user_login()
+        self.assertEqual(user.openstaxuserprofile.openstax_accounts_uuid, '467cea6c-8159-40b1-90f1-e9b0dc26344c')
+
+
+class TestMockRequest(TestCase):
+    def test_create_mock_request(self):
+        req = RequestMock()
+        #print('req: ' + str(req.cookies))
+        #print('req2: ' + str(req.defaults))
+        self.assertEqual(req.cookies, {})
 
