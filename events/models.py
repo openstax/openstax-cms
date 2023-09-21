1c59771ecc45fff137c3f59dcc96a74d2a7da157
Ed Woodward <ecw1@rice.edu>
2023-09-21 07:42:52 -0500

    Comment out events code and remove events models from db (#1505)

--- events/models.py
+++ events/models.py
@@ -1,47 +1,47 @@
 from django.db import models
 from wagtail.core.models import Page
 
-class Event(models.Model):
-    eventbrite_event_id = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.eventbrite_event_id
-
-
-class Session(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.PROTECT)
-    name = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
-    date = models.DateTimeField()
-    location = models.CharField(max_length=255, null=True, blank=True)
-    seats_remaining = models.SmallIntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class Registration(models.Model):
-    session = models.ForeignKey(Session, on_delete=models.PROTECT)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    registration_email = models.EmailField(max_length=255)
-    checked_in = models.DateTimeField(blank=True, null=True)
-
-    @property
-    def full_name(self):
-        return '%s %s' % (self.first_name, self.last_name)
-
-    def __str__(self):
-        return self.full_name
-
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            # reduce the number of seats on the session if this is a new registration
-            self.session.seats_remaining = self.session.seats_remaining - 1
-            self.session.save()
-        super(Registration, self).save(*args, **kwargs)
-
-
-class CreatorFestSession(Page):
-    pass
+# class Event(models.Model):
+#     eventbrite_event_id = models.CharField(max_length=255)
+#
+#     def __str__(self):
+#         return self.eventbrite_event_id
+#
+#
+# class Session(models.Model):
+#     event = models.ForeignKey(Event, on_delete=models.PROTECT)
+#     name = models.CharField(max_length=255)
+#     description = models.TextField(null=True, blank=True)
+#     date = models.DateTimeField()
+#     location = models.CharField(max_length=255, null=True, blank=True)
+#     seats_remaining = models.SmallIntegerField()
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Registration(models.Model):
+#     session = models.ForeignKey(Session, on_delete=models.PROTECT)
+#     first_name = models.CharField(max_length=255)
+#     last_name = models.CharField(max_length=255)
+#     registration_email = models.EmailField(max_length=255)
+#     checked_in = models.DateTimeField(blank=True, null=True)
+#
+#     @property
+#     def full_name(self):
+#         return '%s %s' % (self.first_name, self.last_name)
+#
+#     def __str__(self):
+#         return self.full_name
+#
+#     def save(self, *args, **kwargs):
+#         if not self.pk:
+#             # reduce the number of seats on the session if this is a new registration
+#             self.session.seats_remaining = self.session.seats_remaining - 1
+#             self.session.save()
+#         super(Registration, self).save(*args, **kwargs)
+#
+#
+# class CreatorFestSession(Page):
+#     pass
 
--- events/views.py
+++ events/views.py
@@ -6,25 +6,25 @@ from django.http import JsonResponse
 
 from rest_framework import viewsets
 
-from .models import Session, Registration
-from .serializers import SessionSerializer, RegistrationSerializer
-from .functions import check_eventbrite_registration
+#from .models import Session, Registration
+#from .serializers import SessionSerializer, RegistrationSerializer
+#from .functions import check_eventbrite_registration
 
-def check_reg_status(request):
-    email = request.GET.get('email', None)
-    session_registrations = Registration.objects.filter(registration_email__iexact=email)
-
-    response = OrderedDict(eventbrite_registered=check_eventbrite_registration(email),
-                           session_registered=session_registrations.exists())
-
-    return JsonResponse(response)
-
-
-class SessionViewSet(viewsets.ModelViewSet):
-    queryset = Session.objects.all()
-    serializer_class = SessionSerializer
-
-
-class RegistrationViewSet(viewsets.ModelViewSet):
-    queryset = Registration.objects.all()
-    serializer_class = RegistrationSerializer
+# def check_reg_status(request):
+#     email = request.GET.get('email', None)
+#     session_registrations = Registration.objects.filter(registration_email__iexact=email)
+#
+#     response = OrderedDict(eventbrite_registered=check_eventbrite_registration(email),
+#                            session_registered=session_registrations.exists())
+#
+#     return JsonResponse(response)
+#
+#
+# class SessionViewSet(viewsets.ModelViewSet):
+#     queryset = Session.objects.all()
+#     serializer_class = SessionSerializer
+#
+#
+# class RegistrationViewSet(viewsets.ModelViewSet):
+#     queryset = Registration.objects.all()
+#     serializer_class = RegistrationSerializer
