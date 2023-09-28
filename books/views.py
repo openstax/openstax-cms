001518fb1ccd1ed449d58d71c67b92598bfb17fa
ed woodward <ecw1@rice.edu>
2023-09-28 10:42:29 -0500

    Return NotFound error with message for retired books

--- books/views.py
+++ books/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 from .models import BookIndex, Book
 from .serializers import FacultyResourcesSerializer
@@ -19,6 +20,9 @@ def book_index(request):
 def book_detail(request, slug):
     try:
         page = Book.objects.get(slug=slug)
+        if page.book_state == 'retired':
+            raise NotFound('This book is retired. The latest version can be found at https://openstax.org')
+
         return redirect('/apps/cms/api/v2/pages/{}/'.format(page.pk))
     except Book.DoesNotExist:
         raise Http404("Book does not exist.")
@@ -30,6 +34,9 @@ class ResourcesViewSet(viewsets.ViewSet):
         slug = request.GET.get('slug', False)
         queryset = Book.objects.filter(slug=slug)
         if queryset:
+            if queryset[0].book_state == 'retired':
+                raise NotFound('This book is retired. The latest version can be found at https://openstax.org')
+
             serializer = FacultyResourcesSerializer(queryset[0], context={'request': request})
             return Response(serializer.data)
         else:
