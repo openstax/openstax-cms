0e923d73b27aa6998c6bfeaeb2e09851d75f64ce
Michael Harrison <volo@rice.edu>
2024-02-06 13:31:00 -0600

    revert removing oxauth models

--- oxauth/models.py
+++ oxauth/models.py
@@ -1,4 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+# TODO: This can be removed, it's not being used but will do in another PR because it causing deployment issues.
 class OpenStaxUserProfile(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    openstax_accounts_id = models.IntegerField()
+    openstax_accounts_uuid = models.UUIDField()
+
+    def __str__(self):
+        return self.user.username
