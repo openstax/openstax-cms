9a7a5f521fc9c98038fc8fd13e6485de2b550092
Michael Harrison <volo@rice.edu>
2024-02-23 11:29:51 -0600

    Clarify Salesforce fields and add help text to describe what they are for

--- books/models.py
+++ books/models.py
@@ -538,8 +538,9 @@ class Book(Page):
     book_uuid = models.CharField(
         max_length=255, help_text="collection.xml UUID. Should be same as cnx id.",
         blank=True, null=True)
-    salesforce_abbreviation = models.CharField(max_length=255, blank=True, null=True)
-    salesforce_name = models.CharField(max_length=255, blank=True, null=True)
+    salesforce_abbreviation = models.CharField(max_length=255, blank=True, null=True, help_text='This should match the Books Name from Salesforce.')
+    salesforce_name = models.CharField(max_length=255, blank=True, null=True,
+                                       help_text='This is the name shown on interest/adoption forms and used in Partner filtering. The website only shows unique values from here, so it is possible to combine books for forms')
     salesforce_book_id = models.CharField(max_length=255, blank=True, null=True,
                                           help_text='No tracking and not included on adoption and interest forms if left blank)')
     updated = models.DateTimeField(blank=True, null=True, help_text='Late date web content was updated')
@@ -759,9 +760,8 @@ class Book(Page):
         FieldPanel('book_state'),
         FieldPanel('cnx_id'),
         FieldPanel('book_uuid'),
-        FieldPanel('salesforce_abbreviation'),
-        FieldPanel('salesforce_name'),
-        FieldPanel('salesforce_book_id'),
+        FieldPanel('salesforce_abbreviation', label='Subject Book Name'),
+        FieldPanel('salesforce_name', label='Name displayed on website forms'),
         FieldPanel('updated'),
         FieldPanel('publish_date'),
         InlinePanel('book_subjects', label='Subjects'),
