caa37bd5dee48bb6c9bec0324b9cba1aec6161f3
Michael Harrison <volo@rice.edu>
2024-02-23 11:36:38 -0600

    wrong param name

--- books/models.py
+++ books/models.py
@@ -540,6 +540,8 @@ class Book(Page):
         blank=True, null=True)
     salesforce_abbreviation = models.CharField(max_length=255, blank=True, null=True, label='Subject Book Name', help_text='This should match the Books Name from Salesforce.')
     salesforce_name = models.CharField(max_length=255, blank=True, null=True, label='Name displayed on website forms',
+    salesforce_abbreviation = models.CharField(max_length=255, blank=True, null=True, verbose_name='Subject Book Name', help_text='This should match the Books Name from Salesforce.')
+    salesforce_name = models.CharField(max_length=255, blank=True, null=True, verbose_name='Name displayed on website forms',
                                        help_text='This is the name shown on interest/adoption forms and used in Partner filtering. The website only shows unique values from here, so it is possible to combine books for forms')
     salesforce_book_id = models.CharField(max_length=255, blank=True, null=True,
                                           help_text='No tracking and not included on adoption and interest forms if left blank)')
