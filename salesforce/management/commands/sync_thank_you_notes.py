64bc3fe1116df8fc47848a2c2bbfa9560790d816
Michael Volo <volo@rice.edu>
2025-01-07 16:04:16 -0600

    higher confidence, add cron

--- salesforce/management/commands/sync_thank_you_notes.py
+++ salesforce/management/commands/sync_thank_you_notes.py
@@ -11,35 +11,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         new_thank_you_notes = ThankYouNote.objects.filter(salesforce_id="")
 
-        with Salesforce() as sf:
-            # fetch schools to do a fuzzy match on with thank you note manually inputted names
-            school_list = {school.name: school.salesforce_id for school in School.objects.all()}
+        # fetch schools to do a fuzzy match on with thank you note manually inputted names
+        school_list = {school.name: school.salesforce_id for school in School.objects.all()}
 
+        with Salesforce() as sf:
             num_created = 0
             for note in new_thank_you_notes:
-                if note.institution:
-                    best_match, score, _ = process.extractOne(note.institution, school_list.keys(), scorer=fuzz.WRatio, processor=utils.default_process)
+                account_id = school_list["Find Me A Home"]
 
-                    account_id = school_list["Find Me A Home"]
-                    if score > 90:  # found a good match on school name, use that to populate related school in SF
+                if note.institution:
+                    school_string = note.institution
+                    filtered_choices = [name for name in school_list.keys() if name.lower().startswith(school_string.lower())]
+                    best_match, score, match_key = process.extractOne(school_string, filtered_choices, scorer=fuzz.partial_ratio, processor=utils.default_process())
+
+                    if score > 99:  # found a good match on school name, use that to populate related school in SF
                         account_id = school_list[best_match]
 
-                    response = sf.Thank_You_Note__c.create(
-                        {'Name': f"{note.first_name} {note.last_name} - {note.created}",
-                         'Message__c': note.thank_you_note,
-                         'First_Name__c': note.first_name,
-                         'Last_Name__c': note.last_name,
-                         'Email_Address__c': note.contact_email_address,
-                         'Institution__c': note.institution,
-                         'Source__c': note.source,
-                         'Consent_to_Share__c': note.consent_to_share_or_contact,
-                         'Submitted_Date__c': note.created.strftime('%Y-%m-%d'),
-                         'Related_Account__c': account_id
-                         }
-                    )
+                response = sf.Thank_You_Note__c.create(
+                    {'Name': f"{note.first_name} {note.last_name} - {note.created}",
+                     'Message__c': note.thank_you_note,
+                     'First_Name__c': note.first_name,
+                     'Last_Name__c': note.last_name,
+                     'Email_Address__c': note.contact_email_address,
+                     'Institution__c': note.institution,
+                     'Source__c': note.source,
+                     'Consent_to_Share__c': note.consent_to_share_or_contact,
+                     'Submitted_Date__c': note.created.strftime('%Y-%m-%d'),
+                     'Related_Account__c': account_id
+                     }
+                )
 
-                    note.salesforce_id = response['id']
-                    note.save()
-                    num_created += 1
+                note.salesforce_id = response['id']
+                note.save()
+                num_created += 1
 
             self.stdout.write(self.style.SUCCESS("{} Salesforce Notes Created.".format(num_created)))
