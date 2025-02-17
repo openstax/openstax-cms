8736d143cfc8bb9f7dc6506cd446cf5ea9a2c5c7
Michael Volo <volo@rice.edu>
2025-02-17 17:16:10 -0600

    how long has this been here? 🤦🏼‍♂️ (#1611)
    * how long has this been here? 🤦🏼‍♂️
    
    * remove old tests (these won't run with current config)

--- shared/tests.py
+++ shared/tests.py
@@ -1,15 +1,5 @@
 from django.test import TestCase
 from shared.test_utilities import mock_user_login, RequestMock
-<<<<<<< HEAD
-=======
-
->>>>>>> 8e0972ecb0cb613b6bf9a96425fc017e5fe918e7
-from unittest.mock import MagicMock
-
-class TestMockUserLogin(TestCase):
-    def test_mock_user_login(self):
-        user = mock_user_login()
-        self.assertEqual(user.openstaxuserprofile.openstax_accounts_uuid, '467cea6c-8159-40b1-90f1-e9b0dc26344c')
 
 
 class TestMockRequest(TestCase):
