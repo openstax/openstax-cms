422b5d6becf7b855d4239499aa00e9f3a7da0a4c
Michael Volo <volo@rice.edu>
2025-05-08 11:35:54 -0500

    add is_hs

--- books/models.py
+++ books/models.py
@@ -40,7 +40,6 @@ def prefetch_book_resources(queryset):
     )
 
 def get_book_data(book):
-    """Return the book data dict for a single Book instance, matching BookIndex.books property."""
     has_faculty_resources = hasattr(book, 'prefetched_faculty_resources') and bool(book.prefetched_faculty_resources)
     has_student_resources = hasattr(book, 'prefetched_student_resources') and bool(book.prefetched_student_resources)
     try:
